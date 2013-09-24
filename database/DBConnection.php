5b925ac35c4f1529ed82f5151e700ae4149069c2
Juan Antonio Martinez <jonsito@terra.es>
2013-09-24 15:08:43 +0200

    initial work: apacheconfig,DBConnection,index.php,users.sql, remove mongoose

---
+++ database/DBConnection.php
@@ -0,0 +1,69 @@
+<?php
+define("DBHOST","localhost");
+define("DBNAME","agility");
+
+/**
+ * DB connection handler
+ * This class should only be used from DBConnection objects
+ * @author jantonio
+ *
+ */
+class DBConnection {
+	private static $singleton;
+	private $connections;
+	
+	private function __construct() {
+		$connections=Array();
+	}
+	
+	public static function getInstance() {
+		if (self::$singleton == null) {
+		    self::$singleton = new DBConnection();
+		}
+		return self::$singleton;
+	}
+	
+	public function getConnectionList() { return $this->connections; }
+	
+	public static function getConnection($user,$pass) {
+		// obtiene una instancia del singleton
+		$myDBConnection= self::getInstance();
+		$instances= $myDBConnection->getConnectionList();
+		// si no esta declarado el usuario lo creamos
+		if ($instances[$user]==null) $instances[$user]= array(null,0);
+		// si esta ya conectado, se retorna conexion
+		if ($instances[$user][1]!=0) {
+			$instances[$user][1]++;
+			return $instances[$user][0];
+		}
+		// si no esta conectado, creamos conexion
+		$instances[$user][0]=null;
+		$res = new mysqli(DBHOST,$user,$pass,DBNAME);
+		if (!$res->connect_error) {
+			$instances[$user][1]++;
+			$instances[$user][0]=$res;
+		}
+		return $instances[$user][0];
+	}
+	
+	public static function removeConnection($conn) {
+		// obtiene una instancia del singleton
+		$myDBConnection= self::getInstance();
+		$instances= $myDBConnection->getConnectionList();
+		foreach ($instances as $item) {
+			if ($item[0]==null) continue;
+			if ($item[0]!==$conn) continue;
+			if ($item[1]>0) $item[1]--;
+			// arriving zero means close
+			if ($item[1]==0) {
+				$res = $conn->close();
+				$item[0]=null;
+				return $res;	
+			}
+		}
+		// arriving here means connection not found
+		return FALSE;
+	}
+}
+
+?>
---
+++ index.php
@@ -0,0 +1,45 @@
+<html>
+<head>
+<title>Test</title>
+</head>
+<body>
+<table border="1">
+<?php
+$querystr=
+"
+SELECT Perros.Dorsal, Perros.Nombre, Perros.Categoria, Perros.Grado, Perros.Guia, Guias.Club 
+FROM Perros,Guias
+WHERE ( Perros.Guia = Guias.Nombre )
+ORDER BY Club, Categoria, Nombre
+";
+
+require ("./database/DBConnection.php");
+$conn=DBConnection::getConnection("agility_guest","guest@cachorrera");
+if (!$conn) die("connection error");
+$rs=$conn->query($querystr);
+?>
+<!--  Cabecera de la tabla -->
+<th>
+<?php
+	$fields=$rs->fetch_fields();
+	foreach($fields as $field) {
+		printf("<td>%s</td>",$field->name);
+	}
+?>
+</th>
+<!--  Contenido de la tabla -->
+<?php
+	while($row = $rs->fetch_array() ){
+		printf("<tr>\n");
+		foreach($fields as $field) {
+			printf("<td>%s</td>",$row[$field->name]);
+		}
+		printf("</tr>\n");
+	}
+	// finally close connection
+	$rs->free();
+	DBConnection::removeConnection($conn);
+?>
+</table>
+</body>
+</html>
