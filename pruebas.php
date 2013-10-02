2f4997b2e19823c99d6bdaea7213e4bd013ed8f5
Juan Antonio Martinez <jonsito@terra.es>
2013-10-02 11:46:12 +0200

    Create a separate file for "Pruebas" and "Jornadas" dialogs

--- pruebas.php
+++ pruebas.php
@@ -1,141 +1,21 @@
 <!-- TABLA DE jquery-easyui para listar y editar la BBDD DE Pruebas -->
     
     <!-- DECLARACION DE LA TABLA -->
-    <table id="pruebas-datagrid" class="easyui-datagrid"></table>
+    <table id="pruebas-datagrid" class="easyui-datagrid">     
+    	<!-- BARRA DE TAREAS -->
+    	<div id="pruebas-toolbar">
+    	    <a id="pruebas-newBtn" href="#" class="easyui-linkbutton" iconCls="icon-add" plain="true" onclick="newPrueba()">Nueva prueba</a>
+    	    <a id="pruebas-editBtn" href="#" class="easyui-linkbutton" iconCls="icon-edit" plain="true" onclick="editPrueba()">Editar prueba</a>
+    	    <a id="pruebas-delBtn" href="#" class="easyui-linkbutton" iconCls="icon-remove" plain="true" onclick="destroyPrueba()">Borrar prueba</a>
+    	    <input id="pruebas-search" type="text" onchange="doSearchPrueba()"/> 
+    	    <a id="pruebas-searchBtn" href="#" class="easyui-linkbutton" plain="true" iconCls="icon-search" onclick="doSearchPrueba()">Buscar</a>
+    	    <input id="pruebas-openBox" href="#" type="checkbox" value='1' class="easyui-checkbox" onclick="doSearchPrueba()">Incl. Cerradas</input>
+    	</div>
+    </table>
     
-    <!-- BARRA DE TAREAS -->
-    <div id="pruebas-toolbar">
-        <a id="pruebas-newBtn" href="#" class="easyui-linkbutton" iconCls="icon-add" plain="true" onclick="newPrueba()">Nueva prueba</a>
-        <a id="pruebas-editBtn" href="#" class="easyui-linkbutton" iconCls="icon-edit" plain="true" onclick="editPrueba()">Editar prueba</a>
-        <a id="pruebas-delBtn" href="#" class="easyui-linkbutton" iconCls="icon-remove" plain="true" onclick="destroyPrueba()">Borrar prueba</a>
-        <input id="pruebas-search" type="text" onchange="doSearchPrueba()"/> 
-        <a id="pruebas-searchBtn" href="#" class="easyui-linkbutton" plain="true" iconCls="icon-search" onclick="doSearchPrueba()">Buscar</a>
-        <input id="pruebas-openBox" href="#" type="checkbox" value='1' class="easyui-checkbox" onclick="doSearchPrueba()">Incl. Cerradas</input>
-    </div>
-    
-    <!-- FORMULARIO DE DECLARACION Y MODIFICACION DE PRUEBAS -->
-    <div id="pruebas-dialog" class="easyui-dialog" style="width:400px;height:450px;padding:10px 20px"
-            closed="true" buttons="#pruebas-dlg-buttons">
-        <div class="ftitle">Informaci&oacute;n de la prueba</div>
-        <form id="pruebas-form" method="get" novalidate>
-            <div class="fitem">
-                <label for="Nombre" style="width:250px">Denominaci&oacute;n de la prueba:</label>
-                <input id="pruebas-Nombre" 
-                	name="Nombre" 
-                	type="text" 
-                	class="easyui-validatebox" 
-                	required="true"
-                	style="width:325px" />
-                <input id="pruebas-Viejo" name="Viejo" type="hidden" /> <!-- used to allow operator change prueba's name -->
-            </div>
-            <div class="fitem">
-                <label for="Club">Club:</label>
-                <select id="pruebas-Club" name="Club" class="easyui-combogrid" style="width:250px"/>
-            </div>
-            <div class="fitem">
-                <label for="Ubicacion">Lugar de realizaci&oacute;n</label>
-                <input id="pruebas-Ubicacion" name="Ubicacion" type="text" style="width:250px"/>
-            </div>
-            <div class="fitem">
-                <label for="Triptico">URL del tr&iacute;ptico</label>
-                <input id="pruebas-Triptico" name="Triptico" class="easyui-validatebox" type="text" style="width:250px"/>
-            </div>
-            <div class="fitem">
-                <label for="Cartel">URL del cartel</label>
-                <input id="pruebas-Cartel" name="Cartel" class="easyui-validatebox" type="text" style="width:250px"/>
-            </div>
-            <div class="fitem">
-                <label for="Observaciones">Observaciones:</label>
-                <input id="pruebas-Observaciones" name="Observaciones" type="textarea" style="height:50px;width:250px";/>
-            </div>
-            <div class="fitem"><hr/></div>
-            <div class="fitem">
-                <label for="Cerrada">Prueba Cerrada:</label>
-                <input id="pruebas-Cerrada" name="Cerrada" class="easyui-checkbox" type="checkbox" />
-            </div>
-        </form>
-    </div>
-    
-    <!-- BOTONES DE ACEPTAR / CANCELAR DEL CUADRO DE DIALOGO -->
-    <div id="pruebas-dlg-buttons">
-        <a id="pruebas-okBtn" href="#" class="easyui-linkbutton" iconCls="icon-ok" onclick="savePrueba()">Guardar</a>
-        <a id="pruebas-cancelBtn" href="#" class="easyui-linkbutton" iconCls="icon-cancel" onclick="javascript:$('#pruebas-dialog').dialog('close')">Cancelar</a>
-    </div>
+ 	<?php include_once("dialogs/dlg_pruebas.inc");?>
+ 	<?php include_once("dialogs/dlg_jornadas.inc");?>
 
-     <!-- FORMULARIO DE DECLARACION Y MODIFICACION DE JORNADAS -->
-    <div id="jornadas-dialog" class="easyui-dialog" style="width:350px;height:550px;padding:10px 20px"
-            closed="true" buttons="#jornadas-dlg-buttons">
-        <div class="ftitle">Informaci&oacute;n de la Jornada</div>
-        <form id="jornadas-form" method="get" novalidate>
-            <div class="fitem">
-                <label for="Nombre" style="width:250px">Nombre / Comentarios:</label>
-                <input id="jornadas-Nombre" name="Nombre" type="text" class="easyui-validatebox" style="width:250px"/>
-            </div>
-        	<div class="fitem>
-        		<input type="hidden" name=Jornada/>
-        	</div>
-            <div class="fitem">
-                <label for="Fecha">Fecha</label>
-                <select id="jornadas-Fecha" name="Fecha" class="easyui-datebox" style="width:250px"/>
-            </div>
-            <div class="fitem">
-                <label for="Hora">Hora</label>
-                <input id="jornadas-Hora" name="Hora" class="easyui-timespinner" style="width:250px"/>
-            </div>
-            <div class="fitem">
-            	<label style="width:250px">Mangas a realizar:</label>
-            </div>
-            <div class="fitem">
-                <label for="Grado1" style="width:200px" >Grado 1</label>
-                <input id="jornadas-Grado1" name="Grado1" class="easyui-checkbox" type="checkbox" checked="checked" value="1"/>
-            </div>
-            
-            <div class="fitem">
-                <label for="Grado2" style="width:200px" >Grado 2</label>
-                <input id="jornadas-Grado2" name="Grado2" class="easyui-checkbox" type="checkbox" checked="checked" value="1"/>
-            </div>
-            
-            <div class="fitem">
-                <label for="Grado3" style="width:200px" >Grado 3</label>
-                <input id="jornadas-Grado3" name="Grado3" class="easyui-checkbox" type="checkbox" checked="checked" value="1"/>
-            </div>
-            
-            <div class="fitem">
-                <label for="Equipos" style="width:200px" >Manga por Equipos</label>
-                <input id="jornadas-Equipos" name="Equipos" class="easyui-checkbox" type="checkbox" />
-            </div>
-            
-            <div class="fitem">
-                <label for="PreAgility" style="width:200px" >Pre-Agility</label>
-                <input id="jornadas-PreAgility" name="PreAgility" class="easyui-checkbox" type="checkbox" checked="checked" value="1"/>
-            </div>
-            
-            <div class="fitem">
-                <label for="KO" style="width:200px" >Prueba K.O.</label>
-                <input id="jornadas-KO" name="KO" class="easyui-checkbox" type="checkbox" />
-            </div>
-            <div class="fitem">
-                <label for="Exhibicion" style="width:200px" >Manga de Exhibici&oacute;n</label>
-                <input id="jornadas-Exhibicion" name="Exhibicion" class="easyui-checkbox" type="checkbox" />
-            </div>
-            <div class="fitem">
-                <label for="Otras" style="width:200px" >Otras (no especificada)</label>
-                <input id="jornadas-Otras" name="Otras" class="easyui-checkbox" type="checkbox" />
-            </div>
-            <div class="fitem"><hr/></div>
-            <div class="fitem">
-                <label for="Cerrada" style="width:200px" >Cerrar jornada</label>
-                <input id="jornadas-Cerrada" name="Cerrada" class="easyui-checkbox" type="checkbox" />
-            </div>
-        </form>
-    </div>
-    
-    <!-- BOTONES DE ACEPTAR / CANCELAR DEL CUADRO DE DIALOGO -->
-    <div id="jornadas-dlg-buttons">
-        <a id="jornadas-okBtn" href="#" class="easyui-linkbutton" iconCls="icon-ok" onclick="saveJornada()">Guardar</a>
-        <a id="jornadas-cancelBtn" href="#" class="easyui-linkbutton" iconCls="icon-cancel" onclick="javascript:$('#jornadas-dialog').dialog('close')">Cancelar</a>
-    </div>
-    
     <script type="text/javascript">
     
     	// set up operation header content
@@ -287,82 +167,4 @@
         	onShow: function(){	$(this).tooltip('tip').css({backgroundColor: '#ef0',borderColor: '#444'	});}
         });
 
-        
-        // datos del formulario de nuevo/edit prueba
-        // - declaracion del formulario
-        $('#pruebas-form').form();
-        // - botones
-        $('#pruebas-okBtn').linkbutton();        
-        $('#pruebas-okBtn').tooltip({
-            position: 'top',
-            content: '<span style="color:#000">Aceptar datos. Registrar prueba en la BBDD</span>',
-        	onShow: function(){	$(this).tooltip('tip').css({backgroundColor: '#ef0',borderColor: '#444'	});}
-        });
-        $('#pruebas-cancelBtn').linkbutton();        
-        $('#pruebas-cancelBtn').tooltip({
-            position: 'top',
-            content: '<span style="color:#000">Anular operaci&oacute;n. Cerrar ventana</span>',
-        	onShow: function(){	$(this).tooltip('tip').css({backgroundColor: '#ef0',borderColor: '#444'	});}
-        });
-        
-        // campos del formulario
-        $('#pruebas-dialog').dialog();
-        $('#pruebas-Nombre').validatebox({
-            required: true,
-            validType: 'length[1,255]'
-        });
-        $('#pruebas-Triptico').validatebox({
-            required: false,
-            validType: 'url'
-        });
-        $('#pruebas-Cartel').validatebox({
-            required: false,
-            validType: 'url'
-        });
-        $('#pruebas-Club').combogrid({
-			panelWidth: 350,
-			panelHeight: 200,
-			idField: 'Nombre',
-			textField: 'Nombre',
-			url: 'database/enumerate_Clubes.php',
-			method: 'get',
-			mode: 'remote',
-			required: true,
-			columns: [[
-    			{field:'Nombre',title:'Nombre del club',width:80,align:'right'},
-    			{field:'Provincia',title:'Provincia',width:40,align:'right'},
-			]],
-			multiple: false,
-			fitColumns: true,
-			selectOnNavigation: false
-        });
-
-        // datos del formulario de nuevo/edit jornada
-        $('#jornadas-dialog').dialog();
-        // - declaracion del formulario
-        $('#jornadas-form').form();
-        $('#jornadas-Fecha').datebox({ required: true });
-        var c= $('#jornadas-Fecha').datebox('calendar');
-        c.calendar({ firstDay:1 });
-        $('#jornadas-Hora').timespinner({
-            required: true
-        });
-
-        $('#jornadas-Nombre').validatebox({
-            required: true,
-            validType: 'length[1,255]'
-        });
-        // - botones del formulario de jornadas
-        $('#jornadas-okBtn').linkbutton();        
-        $('#jornadas-okBtn').tooltip({
-            position: 'top',
-            content: '<span style="color:#000">Registrar la entrada en la Base de Datos</span>',
-        	onShow: function(){	$(this).tooltip('tip').css({backgroundColor: '#ef0',borderColor: '#444'	});}
-        });
-        $('#jornadas-cancelBtn').linkbutton();        
-        $('#jornadas-cancelBtn').tooltip({
-            position: 'top',
-            content: '<span style="color:#000">Cancelar la operaci&oacute;n. Cerrar ventana</span>',
-        	onShow: function(){	$(this).tooltip('tip').css({backgroundColor: '#ef0',borderColor: '#444'	});}
-        });
 	</script>
