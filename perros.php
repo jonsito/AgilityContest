7e5734dd8be5ae1a4282cb9fa40a83a448877984
Juan Antonio Martinez <jonsito@terra.es>
2013-10-02 11:19:57 +0200

    Create a separate include file for dialog "Perros"

--- perros.php
+++ perros.php
@@ -8,70 +8,18 @@
     -->
     
     <!-- DECLARACION DE LA TABLA -->
-    <table id="perros-datagrid" class="easyui-datagrid" ></table>
+    <table id="perros-datagrid" class="easyui-datagrid" >
+        <!-- BARRA DE TAREAS -->
+    	<div id="perros-toolbar">
+    	    <a id="perros-newBtn" href="#" class="easyui-linkbutton" iconCls="icon-add" plain="true" onclick="newDog()">Nuevo Perro</a>
+    	    <a id="perros-editBtn" href="#" class="easyui-linkbutton" iconCls="icon-edit" plain="true" onclick="editDog()">Editar Perro</a>
+    	    <a id="perros-delBtn" href="#" class="easyui-linkbutton" iconCls="icon-remove" plain="true" onclick="destroyDog()">Borrar Perro</a>
+    	    <input id="perros-search" type="text" onchange="doSearchPerro()"/> 
+    	    <a id="perros-searchBtn" href="#" class="easyui-linkbutton" plain="true" iconCls="icon-search" onclick="doSearchPerro()">Buscar</a>
+    	</div>
+    </table>
     
-    <!-- BARRA DE TAREAS -->
-    <div id="perros-toolbar">
-        <a id="perros-newBtn" href="#" class="easyui-linkbutton" iconCls="icon-add" plain="true" onclick="newDog()">Nuevo Perro</a>
-        <a id="perros-editBtn" href="#" class="easyui-linkbutton" iconCls="icon-edit" plain="true" onclick="editDog()">Editar Perro</a>
-        <a id="perros-delBtn" href="#" class="easyui-linkbutton" iconCls="icon-remove" plain="true" onclick="destroyDog()">Borrar Perro</a>
-        <input id="perros-search" type="text" onchange="doSearchPerro()"/> 
-        <a id="perros-searchBtn" href="#" class="easyui-linkbutton" plain="true" iconCls="icon-search" onclick="doSearchPerro()">Buscar</a>
-        </div>
-    
-    <!-- FORMULARIO DE ALTA/BAJA/MODIFICACION DE LA BBDD DE PERROS -->
-    <div id="perros-dialog" class="easyui-dialog" style="width:500px;height:375px;padding:10px 20px"
-            closed="true" buttons="#perros-dlg-buttons">
-        <div class="ftitle">Informaci&oacute;n del perro</div>
-        <form id="perros-form" method="get" novalidate>
-            <div class="fitem">
-                <label for="Nombre">Nombre:</label>
-                <input id="perros-Nombre" 
-                	name="Nombre" 
-                	type="text" 
-                	class="easyui-validatebox" 
-                	required="true"/>
-                <input name="Dorsal" type="hidden" /> <!-- hide dorsal, as only used for edit and is readonly -->
-            </div>
-            <div class="fitem">
-                <label for="Raza">Raza:</label>
-                <input id="perros-Raza" class="easyui-validatebox" name="Raza" type="text" />
-            </div>
-            <div class="fitem">
-                <label for="LOE_RRC">Num. LOE / RRC:</label>
-                <input id="perros-LOE_RRC" class="easyui-validatebox" name="LOE_RRC" type="text" />
-            </div>
-            <div class="fitem">
-                <label for="Licencia">Num. Licencia:</label>
-                <input id="perros-Licencia" class="easyui-validatebox" name="Licencia" type="text" />
-            </div>
-            <div class="fitem">
-                <label for="Categoria">Categor&iacute;a:</label>
-                <select id="perros-Categorias_Perro" 
-                		name="Categoria" 
-                		class="easyui-combobox" 
-                		style="width:155px"
-                		required="true" />
-            </div>
-            <div class="fitem">
-                <label for="Grado">Grado:</label>
-                <select id="perros-Grados_Perro" 
-                		name="Grado" 
-                		class="easyui-combobox" 
-                		style="width:155px"
-                		required="true" />
-            </div>
-            <div class="fitem">
-                <label for="Guia">Nombre del Gu&iacute;a:</label>
-                <select id="perros-Guia" name="Guia" class="easyui-combogrid" style="width:300px"/>
-            </div>
-        </form>
-    </div>
-    <!-- BOTONES DE ACEPTAR / CANCELAR DEL CUADRO DE DIALOGO -->
-    <div id="perros-dlg-buttons">
-        <a id="perros-okBtn" href="#" class="easyui-linkbutton" iconCls="icon-ok" onclick="saveDog()">Guardar</a>
-        <a id="perros-cancelBtn" href="#" class="easyui-linkbutton" iconCls="icon-cancel" onclick="javascript:$('#perros-dialog').dialog('close')">Cancelar</a>
-    </div>
+	<?php include_once("dialogs/dlg_perros.inc"); ?>
     
     <script type="text/javascript">
     
@@ -141,72 +89,4 @@
         	onShow: function(){	$(this).tooltip('tip').css({backgroundColor: '#ef0',borderColor: '#444'	});
         	}
     	});
-        // datos del formulario de nuevo/edit perros
-        // - declaracion del formulario
-        $('#perros-form').form();
-        // - botones
-        $('#perros-okBtn').linkbutton();    
-        $('#perros-okBtn').tooltip({
-        	position: 'top',
-        	content: '<span style="color:#000">Aceptar datos y actualizar la BBDD</span>',
-        	onShow: function(){	$(this).tooltip('tip').css({backgroundColor: '#ef0',borderColor: '#444'	});
-        	}
-    	});
-        $('#perros-cancelBtn').linkbutton();    
-        $('#perros-cancelBtn').tooltip({
-        	position: 'top',
-        	content: '<span style="color:#000">Cancelar edicion. Cerrar ventana</span>',
-        	onShow: function(){	$(this).tooltip('tip').css({backgroundColor: '#ef0',borderColor: '#444'	});
-        	}
-    	});
-        // campos del formulario
-        $('#perros-dialog').dialog();
-        $('#perros-Nombre').validatebox({
-            required: true,
-            validType: 'length[1,255]'
-        });
-        $('#perros-Grados_Perro').combobox({
-    		panelHeight: 'auto',
-        	valueField:'Grado',
-        	textField:'Comentarios',
-        	method: 'get',
-        	mode: 'remote',
-        	url:'database/enumerate_Grados_Perro.php',
-        	// TODO: this should work. study why doesn't
-    		onLoadSuccess: function(data){
-    			for(var i=0; i<data.length; i++){
-    				var row = data[i];
-    				// the row with 'selected' property set to true will be acted as the selected row
-    				if (row.selected){  
-        				// alert('selected value is: '+row.Grado);
-    					$(this).combobox('setValue',row.Grado);
-    				}
-    			}
-    		}
-        });
-        $('#perros-Categorias_Perro').combobox({
-			panelHeight: 'auto',
-    		valueField:'Categoria',
-    		textField:'Observaciones',
-    		method: 'get',
-    		mode: 'remote',
-    		url:'database/enumerate_Categorias_Perro.php'
-        });
-        $('#perros-Guia').combogrid({
-			panelWidth: 350,
-			panelHeight: 200,
-			idField: 'Nombre',
-			textField: 'Nombre',
-			url: 'database/enumerate_GuiasClub.php',
-			method: 'get',
-			mode: 'remote',
-			required: true,
-			columns: [[
-    			{field:'Nombre',title:'Nombre del gu&iacute;a',width:80,align:'right'},
-    			{field:'Club',title:'Club',width:40,align:'right'},
-			]],
-			multiple: false,
-			fitColumns: true,
-			selectOnNavigation: false
-        });
 	</script>
