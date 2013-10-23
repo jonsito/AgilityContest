c8d94ec7315bb3e72beee261c6075c4b3581cbee
Juan Antonio Martinez <jonsito@terra.es>
2013-10-23 13:36:29 +0200

    fix proper store of workingData.{prueba,jornada}

--- competicion2.php
+++ competicion2.php
@@ -1,5 +1,5 @@
 <script type="text/javascript">
-        $('#Header_Operation').html('<p>Desarrollo de la prueba</p>');
+        $('#Header_Operation').html('<p>Desarrollo de la jornada</p>');
 </script>
 <h1>Loren Ipsum dolor sit amet</h1>
 <p>
