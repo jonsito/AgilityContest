d22c1234b0d81d94fe4a29f91691cecc507b7bcd
Juan Antonio Martinez <jonsito@gmail.com>
2014-01-15 18:42:25 +0100

    fix some warnings on html tags

--- main.php
+++ main.php
@@ -2,5 +2,5 @@
         $('#Header_Operation').html('<p></p>');
 </script>
 <p>
-<img class="mainpage" src="images/equipo_welpe.jpg" alt="El equipo de W.E.L.P.E." width="800" heigt="400"/>
+<img class="mainpage" src="images/equipo_welpe.jpg" alt="El equipo de W.E.L.P.E." width="800" height="400"/>
 </p>
