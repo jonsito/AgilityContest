b589189c9978e58b14cf7a0a0ebc50ec5f734c49
Juan Antonio Martinez <jonsito@terra.es>
2014-01-15 09:46:36 +0100

    Integrate datosmanga panel into competition as a loadable panel. Some workings in look&feel on datos manga panel

--- infomanga.php
+++ infomanga.php
@@ -1,14 +1,8 @@
-<!DOCTYPE html>
-<html>
-<head>
-<meta charset="UTF-8">
-<title>Insert title here</title>
-</head>
-<body>
-<div data-options="region:'center',title:'Datos de la manga'" style="width:600px;font-size:8pt;">
-		<form id="competicion-formdatosmanga">
-		<input type="hidden" id="dmanga_Manga" name="Manga"/>
-		<table id="competicion-tabladatosmanga">
+<!-- Formulario que contiene los datos de una manga -->
+
+<form id="competicion-formdatosmanga">
+	<input type="hidden" id="dmanga_Manga" name="Manga"/>
+	<table id="competicion-tabladatosmanga">
 		<tr> <!-- fila 0: datos de los jueces -->
 			<td>Juez 1:</td>
 			<td colspan="4"><input id="dmanga_Juez1" type="text" name="Juez1"></td>
@@ -18,15 +12,15 @@
 		<tr> <!-- fila 1 tipos de recorrido -->
 			<td colspan="2">Recorridos: </td>
 			<td colspan="3">
-				<input type="radio" id="dmanga_Recorrido_0" name="Recorrido" value="0" checked="checked">
+				<input type="radio" id="dmanga_Recorrido_0" name="Recorrido" value="0" onClick="setRecorridos();"/>
 				<label for="dmanga_Recorrido_0">Recorrido com&uacute;n</label>
 			</td>
 			<td colspan="3">
-				<input type="radio" id="dmanga_Recorrido_1" name="Recorrido" value="1">
+				<input type="radio" id="dmanga_Recorrido_1" name="Recorrido" value="1" onClick="setRecorridos();"/>
 				<label for="dmanga_Recorrido_1">Std / Mini-Midi</label>
 			</td>
 			<td colspan="3">
-				<input type="radio" id="dmanga_Recorrido_2" name="Recorrido" value="2">
+				<input type="radio" id="dmanga_Recorrido_2" name="Recorrido" value="2" onClick="setRecorridos();"/>
 				<label for="dmanga_Recorrido_2">Std / Midi / Mini</label>
 			</td>
 		</tr>
@@ -39,8 +33,8 @@
 		</tr>
 		<tr> <!-- fila 3: recorrido comun datos standard -->
 			<td>Standard</td>
-			<td><input type="text" id="dmanga_DistL" name="DistanciaL" size="4" value="0"/></td>
-			<td><input type="text" id="dmanga_ObstL" name="ObstaculosL" size="4" value="0"/></td>
+			<td><input type="text" id="dmanga_DistL" name="DistanciaL" size="4" value="0" onChange="setRecorridos();"/></td>
+			<td><input type="text" id="dmanga_ObstL" name="ObstaculosL" size="4" value="0" onChange="setRecorridos();"/></td>
 			<!-- datos para TRS standard -->
 			<td colspan="2"> 
 				<select id="dmanga_TRS_L_Tipo" name="TRS_L_Tipo">
@@ -73,8 +67,8 @@
 		</tr>
 		<tr> <!-- fila 4: recorrido std / mini+midi datos midi -->
 			<td>Medium</td>
-			<td><input type="text" id="dmanga_DistM" name="DistanciaM" size="4" value="0"/></td>
-			<td><input type="text" id="dmanga_ObstM" name="ObstaculosM" size="4" value="0"/></td>
+			<td><input type="text" id="dmanga_DistM" name="DistanciaM" size="4" value="0" onChange="setRecorridos();"/></td>
+			<td><input type="text" id="dmanga_ObstM" name="ObstaculosM" size="4" value="0" onChange="setRecorridos();"/></td>
 			<!-- datos para TRS medium -->
 			<td colspan="2"> 
 				<select id="dmanga_TRS_M_Tipo" name="TRS_M_Tipo">
@@ -108,8 +102,8 @@
 		</tr>
 		<tr> <!-- fila 5: recorrido std / mini / midi + datos mini -->
 			<td>Small</td>
-			<td><input type="text" id="dmanga_DistS" name="DistanciaS" size="4" value="0"/></td>
-			<td><input type="text" id="dmanga_ObstS" name="ObstaculosS" size="4" value="0"/></td>
+			<td><input type="text" id="dmanga_DistS" name="DistanciaS" size="4" value="0" onChange="setRecorridos();"/></td>
+			<td><input type="text" id="dmanga_ObstS" name="ObstaculosS" size="4" value="0" onChange="setRecorridos();"/></td>
 			<!-- datos para TRS small -->
 			<td colspan="2"> 
 				<select id="dmanga_TRS_S_Tipo" name="TRS_S_Tipo">
@@ -144,24 +138,57 @@
 		</tr>
 		<tr> <!-- fila 6: observaciones -->
 			<td colspan="2">Observaciones</td>
-			<td colspan="11"><input type="text" id="dmanga_Observaciones" name="Observaciones" size="64" value=""/></td>
+			<td colspan="11"><input type="text" id="dmanga_Observaciones" name="Observaciones" size="70" value=""/></td>
 		</tr>
 		<tr> <!-- fila 7: manga cerrada. botones reset y save -->
 			<td colspan="2">
 				<label for="dmanga_Cerrada">Cerrar manga</label>
 				<input type="checkbox" id="dmanga_Cerrada" name="Cerrada" value="1">
 			</td>
-			<td colspan="2">&nbsp;</td>
+			<td colspan="3">&nbsp;</td>
 			<td>
 				<input type="button" id="dmanga_Restaurar" name="Restaurar" value="Restaurar">
 			</td>
-			<td colspan="3">&nbsp;</td>
+			<td colspan="2">&nbsp;</td>
 			<td>
 				<input type="button" id="dmanga_Guardar" name="Guardar" value="Guardar">
 			</td>
 		</tr>
-		</table>
-		</form>
-	</div> <!-- datos de la manga -->
-</body>
-</html>
+	</table>
+</form>
+<script type="text/javascript">
+
+function setRecorridos() {
+	var val=$("input:radio[name=Recorrido]:checked").val(); 
+	switch (val) {
+	case '0':
+		var distl=$('#dmanga_DistL').val();
+		var obstl=$('#dmanga_ObstL').val();
+		$('#dmanga_DistM').attr('disabled',true);
+		$('#dmanga_DistM').val(distl);
+		$('#dmanga_ObstM').attr('disabled',true);
+		$('#dmanga_ObstM').val(obstl);
+		$('#dmanga_DistS').attr('disabled',true);
+		$('#dmanga_DistS').val(distl);
+		$('#dmanga_ObstS').attr('disabled',true);
+		$('#dmanga_ObstS').val(obstl);
+		break;
+	case '1':
+		var distm=$('#dmanga_DistM').val();
+		var obstm=$('#dmanga_ObstM').val();
+		$('#dmanga_DistM').removeAttr('disabled');
+		$('#dmanga_ObstM').removeAttr('disabled');
+		$('#dmanga_DistS').attr('disabled',true);
+		$('#dmanga_DistS').val(distm);
+		$('#dmanga_ObstS').attr('disabled',true);
+		$('#dmanga_ObstS').val(obstm);
+		break;
+	case '2':
+		$('#dmanga_DistM').removeAttr('disabled');
+		$('#dmanga_ObstM').removeAttr('disabled');
+		$('#dmanga_DistS').removeAttr('disabled');
+		$('#dmanga_ObstS').removeAttr('disabled');
+		break;
+	}
+}
+</script>
