d846830f7b41e9192b6c7961811deed29f7835d7
Juan Antonio Martinez <jonsito@terra.es>
2014-02-17 09:00:46 +0100

    Synchronize github and dropbox remote repositories

--- doc/database.php
+++
@@ -1,4 +0,0 @@
-<script type="text/javascript">
-        $('#Header_Operation').html('<p>Estructura de la Base de Datos</p>');
-</script>
-<img src="/agility/images/database.png" alt="database.png"/>
--- main.php
+++
@@ -1,6 +0,0 @@
-<script type="text/javascript">
-        $('#Header_Operation').html('<p></p>');
-</script>
-<p>
-<img class="mainpage" src="images/equipo_welpe.jpg" alt="El equipo de W.E.L.P.E." width="800" height="400"/>
-</p>
