f17aedc30aead2382374f49e4f1a32d0c2ac4a22
jonsito <jonsito@gmail.com>
2014-08-24 19:13:19 +0200

    add print_entradaDeDatos() compact style

--- agility/pdf/print_entradaDeDatos.php
+++ agility/pdf/print_entradaDeDatos.php
@@ -21,7 +21,7 @@ class PDF extends FPDF {
 	protected $prueba; // datos de la prueba
 	protected $jornada; // datos de la jornada
 	protected $manga; // datos de la manga
-	protected $orden; // orden de salida
+	protected $numrows; // formato del pdf 0:1 1:5 2:15 perros/pagina
 	protected $categoria; // categoria que estamos listando
 	protected $myLogger;
 
@@ -39,7 +39,7 @@ class PDF extends FPDF {
 	 * @param {array} $inscritos Lista de inscritos en formato jquery array[count,rows[]]
 	 * @throws Exception
 	 */
-	function __construct($prueba,$jornada,$manga,$orden) {
+	function __construct($prueba,$jornada,$manga,$orden,$numrows) {
 		parent::__construct('Portrait','mm');
 		if ( ($prueba===null) || ($jornada===null) || ($manga===null) || ($orden===null) ) {
 			$this->errormsg="printEntradaDeDatos: either prueba/jornada/ manga/orden data are invalid";
@@ -49,6 +49,7 @@ class PDF extends FPDF {
 		$this->jornada=$jornada;
 		$this->manga=$manga;
 		$this->orden=$orden;
+		$this->numrows=$numrows;
 		$this->categoria="L";
 		$this->myLogger= new Logger("printEntradaDeDatos");
 	}
@@ -100,25 +101,64 @@ class PDF extends FPDF {
 		$this->myLogger->leave();
 	}
 	
-	function writeTableHeader() {
-		$this->myLogger->enter();
-		// Colores, ancho de línea y fuente en negrita de la cabecera de tabla
+	function writeTableCell_compacto($rowcount,$row) {
+		$this->myLogger->trace("imprimiendo datos del idperro: ".$row['Perro']);
+		// cada celda tiene una cabecera con los datos del participante
 		$this->SetFillColor(0,0,255); // azul
+		$this->SetDrawColor(0,0,0); // negro para los recuadros
+		// save cursor position
+		$x=$this->getX();
+		$y=$this->GetY();
+
+		// fase 1: contenido de cada celda de la cabecera
+		// Cell( width,height,message,border,cursor,align,fill)
+		// border 1:0:'[LTRB]'
 		$this->SetTextColor(255,255,255); // blanco
-		$this->SetFont('Arial','B',9); // bold 9px
-		for($i=0;$i<count($this->cellHeader);$i++) {
-			// en la cabecera texto siempre centrado
-			$this->Cell($this->pos[$i],7,$this->cellHeader[$i],1,0,'C',true);
-		}
-		// Restauración de colores y fuentes
-		$this->SetFillColor(224,235,255); // azul merle
+		$this->SetFont('Arial','B',20); // bold 20px
+		$this->Cell(15,14,$row['Dorsal'],		'LTBR',0,'C',true); // display order
+		$this->SetFont('Arial','B',10); // bold 10px
+		$this->Cell(10,7,($row['Celo']!=0)?"Celo":"",'TR',0,'C',true);
+		$this->Cell(15,7,$row['Licencia'],		'TR',0,'C',true);
+		$this->Cell(25,7,$row['Nombre'],		'TR',0,'R',true);
+		$this->Cell(50,7,$row['NombreGuia'],	'TR',0,'R',true);
+		$this->Cell(35,7,$row['NombreClub'],	'TR',0,'R',true);
+		$this->Cell(7, 14,'',	'TRB',0,'L',false);
+		$this->Cell(7, 14,'',	'TRB',0,'L',false);
+		$this->Cell(7, 14,'',	'TRB',0,'L',false);
+		$this->Cell(19, 14,'',  'TRB',0,'L',false);
+
+		// ahora pintamos los nombres de los campos en negro fondo transparente sin borde
+		$this->SetXY($x,$y); // restore cursor position
 		$this->SetTextColor(0,0,0); // negro
-		$this->SetFont('Arial','',9); // remove bold
-		$this->Ln();
-		$this->myLogger->leave();
+		$this->SetFont('Arial','I',8); // italic 8px
+		$this->Cell(15,4,'Dorsal',	'',0,'L',false); // display order
+		$this->Cell(10,4,'Celo',	'',0,'L',false);
+		$this->Cell(15,4,'Licencia','',0,'L',false);
+		$this->Cell(25,4,'Nombre',	'',0,'L',false);
+		$this->Cell(50,4,'Guia',	'',0,'L',false);
+		$this->Cell(35,4,'Club',	'',0,'L',false);
+		$this->Cell(7, 4,'Flt.',	'',0,'C',false);
+		$this->Cell(7, 4,'Toc.',	'',0,'C',false);
+		$this->Cell(7, 4,'Reh.',	'',0,'C',false);
+		$this->Cell(19,4,'Tiempo',  '',0,'C',false);
+		
+		// ahora pintamos recuadritos
+		$this->SetFillColor(224,235,255); // azul merle
+		$this->SetXY($x+15,$y+7); 
+		$this->Cell(15,7,'Faltas','RB',0,'L',false);
+		for ($i=1;$i<=10;$i++) $this->Cell(5,7,$i,'RB',0,'C',(($i&0x01)==0)?false:true);
+		$this->Cell(15,7,'Tocados','RB',0,'L',false);
+		for ($i=1;$i<=5;$i++) $this->Cell(5,7,$i,'RB',0,'C',(($i&0x01)==0)?false:true);
+		$this->Cell(15,7,'Rehúses','RB',0,'L',false);
+		for ($i=1;$i<=3;$i++) $this->Cell(5,7,$i,'RB',0,'C',(($i&0x01)==0)?false:true);
+		$this->Ln(8);
 	}
 	
-	function writeTableCell($rowcount,$row) {
+	function writeTableCell_extendido($rowcount,$row) {
+		
+	}
+	
+	function writeTableCell_normal($rowcount,$row) {
 		$this->myLogger->trace("imprimiendo datos del idperro: ".$row['Perro']);
 		// cada celda tiene una cabecera con los datos del participante
 		$this->SetFillColor(0,0,255); // azul
@@ -180,7 +220,6 @@ class PDF extends FPDF {
 		
 		$this->SetDrawColor(128,0,0);
 		$this->SetLineWidth(.3);
-		
 		// Datos
 		$rowcount=0;
 		foreach($this->orden as $row) {
@@ -192,10 +231,14 @@ class PDF extends FPDF {
 				$rowcount=0;
 			}
 			// REMINDER: $this->cell( width, height, data, borders, where, align, fill)
-			if( ($rowcount%5) == 0 ) { // assume 5 entries per page 
+			if( ($rowcount % $this->numrows) == 0 ) { // assume $numrows entries per page 
 				$this->addPage();
 			}
-			$this->writeTableCell($rowcount,$row);
+			switch($this->numrows) {
+				case 1: $this->writeTableCell_extendido($rowcount,$row);break;
+				case 5: $this->writeTableCell_normal($rowcount,$row);break;
+				case 15: $this->writeTableCell_compacto($rowcount,$row);break;
+			}
 			$rowcount++;
 		}
 		// Línea de cierre
@@ -206,11 +249,12 @@ class PDF extends FPDF {
 
 // Consultamos la base de datos
 try {
-	$myLogger=new Logger("base");
+	$myLogger=new Logger("print_entradaDeDatos");
 	$pruebaid=http_request("Prueba","i",0);
 	$jornadaid=http_request("Jornada","i",0);
 	$mangaid=http_request("Manga","i",0);
-	$myLogger->info("Prueba:$pruebaid Jornada:$jornadaid Manga:$mangaid");
+	$mode=http_request("Mode","i",0);
+	$myLogger->info("Prueba:$pruebaid Jornada:$jornadaid Manga:$mangaid Mode:$mode");
 	
 	// Datos de la prueba
 	$p=new Pruebas("printEntradaDeDatos");
@@ -228,7 +272,7 @@ try {
 	die ("Error accessing database: ".$e.getMessage());
 };
 // Creamos generador de documento
-$pdf = new PDF($prueba,$jornada,$manga,$orden['rows']);
+$pdf = new PDF($prueba,$jornada,$manga,$orden['rows'],$mode);
 $pdf->AliasNbPages();
 $pdf->composeTable();
 $pdf->Output("printEntradaDeDatos.pdf","D"); // "D" means open download dialog
