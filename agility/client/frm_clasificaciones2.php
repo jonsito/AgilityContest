07f422fd77f864e51f5806f2bc4698aa909a1b6a
jonsito <jonsito@gmail.com>
2014-11-08 23:05:13 +0100

    Add copyright notice on files

--- agility/client/frm_clasificaciones2.php
+++ agility/client/frm_clasificaciones2.php
@@ -1,4 +1,20 @@
-<!-- CLASIFICACIONES DE PRUEBA/JORNADA/RONDA -->
+<!-- 
+frm_clasificaciones2.php
+
+Copyright 2013-2014 by Juan Antonio Martinez ( juansgaviota at gmail dot com )
+
+This program is free software; you can redistribute it and/or modify it under the terms 
+of the GNU General Public License as published by the Free Software Foundation; 
+either version 2 of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
+without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with this program; 
+if not, write to the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ -->
+ <!-- CLASIFICACIONES DE PRUEBA/JORNADA/RONDA -->
 <div id="resultados-info" class="easyui-panel" title="Informacion de la Ronda">
 <div id="resultados-infolayout" class="easyui-layout" style="height:175px;">
 	<div data-options="region:'west',title:'Datos de la Prueba',split:true,collapsed:false" style="width:300px;padding:10px;font-size:9px">
--- agility/server/pdf/print_common.php
+++ agility/server/pdf/print_common.php
@@ -1,4 +1,22 @@
 <?php
+/*
+print_common.php
+
+Copyright 2013-2014 by Juan Antonio Martinez ( juansgaviota at gmail dot com )
+
+This program is free software; you can redistribute it and/or modify it under the terms 
+of the GNU General Public License as published by the Free Software Foundation; 
+either version 2 of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
+without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with this program; 
+if not, write to the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+*/
+
+
 define('FPDF_FONTPATH', __DIR__."/font/");
 require_once(__DIR__."/fpdf.php");
 require_once(__DIR__."/../database/tools.php");
