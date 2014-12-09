1a7fdd2c67329a171a2b4e7574020f9484078b1d
jonsito <jonsito@gmail.com>
2014-12-09 17:23:42 +0100

    ClientSide skeleton for admin (users/session/config/tools) frame

--- agility/client/frm_admin.php
+++ agility/client/frm_admin.php
@@ -1,25 +1,14 @@
-<h1>Loren Ipsum dolor sit amet</h1>
-<p>
-Lorem ipsum dolor sit amet, consectetuer adipiscing elit, 
-sed diam nonummy nibh euismod tincidunt ut laoreet dolore 
-magna aliquam erat volutpat. 
-Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper 
-suscipit lobortis nisl ut aliquip ex ea commodo consequat. 
-Duis autem vel eum iriure dolor in hendrerit in vulputate 
-velit esse molestie consequat, vel illum dolore eu feugiat 
-nulla facilisis at vero eros et accumsan et iusto odio 
-dignissim qui blandit praesent luptatum zzril delenit augue 
-duis dolore te feugait nulla facilisi. 
-Nam liber tempor cum soluta nobis eleifend option congue nihil 
-imperdiet doming id quod mazim placerat facer possim assum. 
-Typi non habent claritatem insitam; est usus legentis in iis 
-qui facit eorum claritatem. 
-Investigationes demonstraverunt lectores legere me lius quod 
-ii legunt saepius. Claritas est etiam processus dynamicus, 
-qui sequitur mutationem consuetudium lectorum. 
-Mirum est notare quam littera gothica, quam nunc putamus parum claram, 
-anteposuerit litterarum formas humanitatis per seacula 
-quarta decima et quinta decima. 
-Eodem modo typi, qui nunc nobis videntur parum clari, 
-fiant sollemnes in futurum.
-</p>
+    <div id="admin-tab" class="easyui-tabs" style="width:100%;height:auto;">
+    	<div title="Usuarios" data-options="iconCls:'icon-users'" style="padding:20px;display:none;">
+    	<?php require_once("dialogs/dlg_usuarios.inc")?>
+    	</div>
+    	<div title="Sesiones" data-options="iconCls:'icon-order'" style="padding:20px;display:none;">
+    	<?php require_once("dialogs/dlg_sesiones.inc")?>
+    	</div>
+    	<div title="Configuraci&oacute;n" data-options="iconCls:'icon-setup'" style="padding:20px;display:none;">
+    	<?php require_once("dialogs/dlg_configuracion.inc")?>
+    	</div>
+    	<div title="Utilidades" data-options="iconCls:'icon-tools'" style="padding:20px;display:none;">
+    	<?php require_once("dialogs/dlg_tools.inc")?>
+    	</div>
+    </div>
