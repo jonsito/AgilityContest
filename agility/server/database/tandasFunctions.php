aab2540e3fc93fe0928cd57779f494c8faceace2
Juan Antonio Martinez <jonsito@gmail.com>
2015-01-15 14:44:29 +0100

    add missing tandasFunctions.php file

---
+++ agility/server/database/tandasFunctions.php
@@ -0,0 +1,78 @@
+<?php
+/*
+tandasFunctions.php
+
+Copyright 2013-2015 by Juan Antonio Martinez ( juansgaviota at gmail dot com )
+
+This program is free software; you can redistribute it and/or modify it under the terms 
+of the GNU General Public License as published by the Free Software Foundation; 
+either version 2 of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
+without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with this program; 
+if not, write to the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+*/
+
+
+/** mandatory requires for database and logging */
+require_once(__DIR__."/../tools.php");
+require_once(__DIR__."/../logging.php");
+require_once(__DIR__."/../auth/AuthManager.php");
+require_once(__DIR__."/classes/DBConnection.php");
+require_once(__DIR__."/classes/Tandas.php");
+
+$file="tandasFunctions";
+
+try {
+	$result=null;
+	$am= new AuthManager($file);
+	// retrieve variables
+	$operation=http_request("Operation","s",null);
+	if ($operation===null) 
+		throw new Exception("Call to tandasFunctions without 'Operation' requested");
+	// request prueba and jornada. invoke constructor
+	$p = http_request("Prueba","i",0);
+	$j = http_request("Jornada","i",0);
+	$ot = new Tandas($file,$p,$j);
+	
+	// datos para listados, altas y bajas
+	$td = http_request("Tanda","i",0);
+	$s = http_request("Session","i",1); // session id. default '-- sin asignar --'
+	$a = http_request("Pendientes","i",0);
+	
+	// los siguiente campos se usan para drag and drop
+	$f = http_request("From","i",0);
+	$t = http_request("To","i",0);
+	$w = http_request("Where","i",0); // 0:up 1:down
+	if ( ($p<=0) || ($j<=0) ) 
+		throw new Exception("Call to tandasFunctions with Invalid Prueba:$p or Jornada:$j ID");
+	switch ($operation) {
+		/* TODO: write */
+		/*
+		case "insert":
+		case "update":
+		case "delete":
+		case "populateJornada";
+		case "deleteJornada";
+		*/
+		case "getTandas":$result = $ot->getTandas($s); break;
+		case "getData":	$result = $ot->getData($s,$td,$p); break;
+		case "getDataByTanda": $result = $ot->getDataByTanda($s,$td); break;
+		case "swap": $result = $ot->swap($f,$t); break;
+		case "dnd":	$am->access(PERMS_ASSISTANT); $result = $ot->dragAndDrop($f,$t,$w); break;
+	}
+	// result may contain null (error),  "" success, or (any) data
+	if ($result===null) 
+		throw new Exception($ot->errormsg);
+	if ($result==="") 
+		echo json_encode(array('success'=>true,'insert_id'=>0,'affected_rows'=>0));
+	else echo json_encode($result);
+} catch (Exception $e) {
+	do_log($e->getMessage());
+	echo json_encode(array('errorMsg'=>$e->getMessage()));
+}
+
+?>
