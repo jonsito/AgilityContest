344d12f47aa628e03ea02f36a1785bda091ffccb
Juan Antonio Martinez <jonsito@gmail.com>
2015-02-19 14:52:37 +0100

    frm_pruebas.php: Fix expandRow() issues

--- agility/client/frm_pruebas.php
+++ agility/client/frm_pruebas.php
@@ -57,7 +57,6 @@ if not, write to the Free Software Foundation, Inc., 51 Franklin Street, Fifth F
         
         // datos de la tabla de pruebas
         $('#pruebas-datagrid').datagrid({
-            expandedRow: -1, // added by jamc
         	// propiedades del panel padre asociado
         	fit: true,
         	border: false,
@@ -99,13 +98,7 @@ if not, write to the Free Software Foundation, Inc., 51 Franklin Street, Fifth F
             detailFormatter:function(index,row){
                 return '<div style="padding:2px"><table id="jornadas-datagrid-' + row.ID + '"/></div>';
             },
-            onExpandRow: function(index,row) {
-                var oldRow=$('#pruebas-datagrid').datagrid('options').expandedRow;
-                if (oldRow!=-1) $('#pruebas-datagrid').datagrid('collapseRow',oldRow);
-                $('#pruebas-datagrid').datagrid('options').expandedRow=index;
-                workingData.datosPrueba=row;
-                if (row.ID!=0) showJornadasByPrueba(index,row); 
-            }
+            onExpandRow: function(index,row) { showJornadasByPrueba(index,row); }
             
         }); // end of pruebas-datagrid
         
