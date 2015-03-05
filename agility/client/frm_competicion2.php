05e4221ad9377f2431acd80adea609f8d06e195a
Juan Antonio Martinez <jonsito@gmail.com>
2015-03-05 14:03:15 +0100

    frm_competicion: avoid re-evaluate subwindows before load manga info

--- agility/client/frm_competicion2.php
+++ agility/client/frm_competicion2.php
@@ -117,11 +117,11 @@ $('#competicion-listamangas').datagrid({
         	$('#competicion_infolayout').layout('panel','center').panel('setTitle','<?php _e('Datos de la manga');?> -- '+workingData.nombreManga);
         	// datos del panel lateral con informacion de la manga
         	reload_manga(workingData.manga);
+            // refresh orden de salida/competicion/resultados
+            reloadOrdenSalida();
+            reloadCompeticion();
+            setupResultadosWindow(row.Recorrido);
         });
-        // refresh orden de salida/competicion/resultados
-        reloadOrdenSalida();
-        reloadCompeticion();
-        setupResultadosWindow(row.Recorrido);
     }
 });
 
