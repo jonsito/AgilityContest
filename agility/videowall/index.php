745747bcca6c34c7923bc65e964892c275a407e9
Juan Antonio Martinez <jonsito@gmail.com>
2015-03-30 14:10:43 +0200

    fixes on include paths for upgradeVersion.php

--- agility/videowall/index.php
+++ agility/videowall/index.php
@@ -6,7 +6,7 @@ require_once(__DIR__."/../server/tools.php");
 require_once(__DIR__."/../server/auth/Config.php");
 $config =Config::getInstance();
 // tool to perform automatic upgrades in database when needed
-require_once(__DIR__."/server/Upgrade.php");
+require_once(__DIR__."/../server/upgradeVersion.php");
 ?>
 <!DOCTYPE html>
 <html>
