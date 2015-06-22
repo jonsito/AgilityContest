2e1b08ee0c813c274146cb9a67954752d27b718d
jonsito <jonsito@gmail.com>
2015-06-23 00:50:04 +0200

    Juzge's Assistant forms for Team4 contests DONE

--- agility/server/pdf/print_entradaDeDatosEquipos4.php
+++ agility/server/pdf/print_entradaDeDatosEquipos4.php
@@ -33,16 +33,19 @@ require_once(__DIR__.'/../database/classes/Jornadas.php');
 require_once(__DIR__.'/../database/classes/Equipos.php');
 require_once(__DIR__."/print_common.php");
 
-class EquiposByJornada extends PrintCommon {
+class EntradaDeDatosEquipos4 extends PrintCommon {
 	protected $equipos; // lista de equipos de esta jornada
     protected $perros; // lista de participantes en esta jornada
+    protected $manga; // datos de la manga
+    protected $categoria;
 	
 	// geometria de las celdas
 	protected $cellHeader;
     //                      Dorsal  nombre raza licencia Categoria guia club  celo  observaciones
 	protected $pos	=array( 10,     25,     27,    10,    18,      40,   25,  10,    25);
 	protected $align=array( 'R',    'C',    'R',    'C',  'C',     'R',  'R', 'C',   'R');
-	protected $cat  =array("-" => "","L"=>"Large","M"=>"Medium","S"=>"Small","T"=>"Tiny");
+	protected $cat=
+        array("-" => "","L"=>"Large","M"=>"Medium","S"=>"Small","T"=>"Tiny","LM"=>"Large/Medium","ST"=>"Small/Tiny","MS"=>"Medium/Small");
 	
 	/**
 	 * Constructor
@@ -51,7 +54,7 @@ class EquiposByJornada extends PrintCommon {
      * @param {integer} $manga Manga ID
 	 * @throws Exception
 	 */
-	function __construct($prueba,$jornada) {
+	function __construct($prueba,$jornada,$manga) {
 		parent::__construct('Portrait',"print_entradaDeDatosEquipos4",$prueba,$jornada);
 		if ( ($prueba<=0) || ($jornada<=0) ) {
 			$this->errormsg="print_datosEquipos4: either prueba or jornada data are invalid";
@@ -63,36 +66,46 @@ class EquiposByJornada extends PrintCommon {
             $this->errormsg="print_datosEquipos4: Jornada $jornada has no Team competition declared";
             throw new Exception($this->errormsg);
         }
-		// Datos de equipos de la jornada
-        $m=new Equipos("print_datosEquipos4",$prueba,$jornada);
-        $teams=$m->getTeamsByJornada();
-        // reindexamos por ID y anyadimos un campo extra "Perros" con los perros del equipo
-        $this->equipos=array();
-        foreach ($teams as &$equipo) {
-            $equipo['Perros']=$m->getPerrosByTeam($equipo['ID']);
-            $this->equipos[$equipo['ID']]=$equipo;
+        // guardamos info de la manga
+        $this->manga=$this->myDBObject->__getObject("Mangas",$manga);
+        // Datos del orden de salida de equipos
+        $m = new OrdenSalida("entradaDeDatosEquipos4",$manga);
+        $teams= $m->getTeams();
+        $this->equipos=$teams['rows'];
+        // anyadimos el array de perros del equipo
+        foreach($this->equipos as &$equipo) {$equipo['Perros']=array();}
+        $r= $this->myDBObject->__select("*","Resultados","(Manga=$manga)","","");
+        foreach($r['rows'] as $perro) {
+            foreach($this->equipos as &$equipo) {
+                if ($perro['Equipo']==$equipo['ID']) {
+                    array_push($equipo['Perros'],$perro);
+                    break;
+                }
+            }
         }
-
-        // Datos de los participantes (indexados por ID de perro)
-        $m=new DBObject("print_datosEquipos4");
-        $r=$m->__select("*","Resultados","(Jornada=$jornada)","","");
-        $this->perros=array();
-        foreach($r['rows'] as $item) {
-            $this->perros[intval($item['Perro'])]=$item;
-        }
-        // finalmente internacionalizamos cabeceras
-		$this->cellHeader = 
-				array(_('Dorsal'),_('Nombre'),_('Raza'),_('Lic.'),_('Cat.'),_('Guía'),_('Club'),_('Celo'),_('Observaciones'));
 	}
 	
 	// Cabecera de página
 	function Header() {
 		$this->print_commonHeader(_("Entrada de datos (Equipos-4)"));
-        // pintamos "identificacion de la manga"
+
+        // pintamos datos de la jornada
         $this->SetFont('Arial','B',12); // Arial bold 15
-        $str  = "Jornada: ". $this->jornada->Nombre . " - " . $this->jornada->Fecha;
-        $this->Cell(90,6,$str,0,0,'L',false); // a un lado nombre y fecha de la jornada
-        $this->Ln(6);
+        $str  = $this->jornada->Nombre . " - " . $this->jornada->Fecha;
+        $this->Cell(90,9,$str,0,0,'L',false);
+
+        // pintamos tipo y categoria de la manga
+        $tmanga= Mangas::$tipo_manga[$this->manga->Tipo][1];
+        $categoria=$this->cat[$this->categoria];
+        $str2 = "$tmanga - $categoria";
+        $this->Cell(100,9,$str2,0,0,'R',false); // al otro lado tipo y categoria de la manga
+        $this->Ln(12);
+        // indicamos nombre del operador que rellena la hoja
+        $this->ac_header(2,12);
+        $this->Cell(90,7,'Apunta:','LTBR',0,'L',true);
+        $this->Cell(10,7,'',0,'L',false);
+        $this->Cell(90,7,'Revisa:','LTBR',0,'L',true);
+        $this->Ln();
 	}
 	
 	// Pie de página
@@ -100,7 +113,7 @@ class EquiposByJornada extends PrintCommon {
 		$this->print_commonFooter();
 	}
 	
-	function printTeamInfo($rowcount,$team,$members) {
+	function printTeamInfo($rowcount,$index,$team,$members) {
         // evaluate logos
         $logos=array('null.png','null.png','null.png','null.png');
         if ($team['Nombre']==="-- Sin asignar --") {
@@ -108,47 +121,49 @@ class EquiposByJornada extends PrintCommon {
         } else {
             $count=0;
             foreach($members as $miembro) {
-                $logo=$miembro['Logo'];
+                $logo=$this->getLogoName($miembro['Perro']);
                 if ( ( ! in_array($logo,$logos) ) && ($count<4) ) $logos[$count++]=$logo;
             }
         }
         // posicion de la celda
-        $y=50+15*($rowcount%10);
+        $y=60+22*($rowcount);
         $this->SetXY(10,$y);
         // caja de datos de perros
         $this->ac_header(2,16);
-        $this->Cell(12,12,1+$rowcount,'LTBR',0,'C',true);
-        $this->Cell(50,12,"","TBR",0,'C',true);
+        $this->Cell(12,18,1+$index,'LTB',0,'C',true);
+        $this->Cell(48,18,"","TBR",0,'C',true);
         $this->SetY($y+1);
         $this->ac_header(2,16);
         foreach($members as $id => $perro) {
             $this->SetX(22);
-            $this->ac_row($id,6);
-            $this->Cell(6,2.5,$perro['Dorsal'],'LTBR',0,'L',true);
-            $this->Cell(13,2.5,$perro['Nombre'],'LTBR',0,'C',true);
-            $this->Cell(28,2.5,$perro['NombreGuia'],'LTBR',0,'R',true);
-            $this->Ln(2.5);
+            $this->ac_row($id,8);
+            $this->Cell(6,4,$perro['Dorsal'],'LTBR',0,'L',true);
+            $this->SetFont('Arial','B',8);
+            $this->Cell(13,4,$perro['Nombre'],'LTBR',0,'C',true);
+            $this->SetFont('Arial','',7);
+            $this->Cell(28,4,$perro['NombreGuia'],'LTBR',0,'R',true);
+            $this->Ln(4);
         }
         // caja de datos del equipo
         $this->SetXY(70,$y);
-        $this->ac_header(1,10);
-        $this->Cell(130,12,"","LTBR",0,'C',true);
+        $this->ac_header(1,14);
+        $this->Cell(130,18,"","LTBR",0,'C',true);
         $this->SetXY(71,$y+1);
-        $this->Cell(10,2.5,$this->Image(__DIR__.'/../../images/logos/'.$logos[0],$this->getX(),$this->getY(),2.5),"",0,'C',($logos[0]==='null.png')?true:false);
-        $this->Cell(10,2.5,$this->Image(__DIR__.'/../../images/logos/'.$logos[1],$this->getX(),$this->getY(),2.5),"",0,'C',($logos[1]==='null.png')?true:false);
-        $this->Cell(10,2.5,$this->Image(__DIR__.'/../../images/logos/'.$logos[2],$this->getX(),$this->getY(),2.5),"",0,'C',($logos[2]==='null.png')?true:false);
-        $this->Cell(10,2.5,$this->Image(__DIR__.'/../../images/logos/'.$logos[3],$this->getX(),$this->getY(),2.5),"",0,'C',($logos[3]==='null.png')?true:false);
-        $this->Cell(80,2.5,$team['Nombre'],'',0,'R',true);
-        $this->Cell(8,2.5,'','',0,'',true); // empty space at right of page
+        $this->Cell(10,5,$this->Image(__DIR__.'/../../images/logos/'.$logos[0],$this->getX(),$this->getY(),5),"",0,'C',($logos[0]==='null.png')?true:false);
+        $this->Cell(10,5,$this->Image(__DIR__.'/../../images/logos/'.$logos[1],$this->getX(),$this->getY(),5),"",0,'C',($logos[1]==='null.png')?true:false);
+        $this->Cell(10,5,$this->Image(__DIR__.'/../../images/logos/'.$logos[2],$this->getX(),$this->getY(),5),"",0,'C',($logos[2]==='null.png')?true:false);
+        $this->Cell(10,5,$this->Image(__DIR__.'/../../images/logos/'.$logos[3],$this->getX(),$this->getY(),5),"",0,'C',($logos[3]==='null.png')?true:false);
+        $this->Cell(80,5,$team['Nombre'],'',0,'R',true);
+        $this->Cell(8,5,'','',0,'',true); // empty space at right of page
         $this->Ln();
         // caja de faltas/rehuses/tiempos
         $this->ac_SetFillColor("#ffffff"); // white background
-        $this->SetXY(71,4+$y);
-        $this->Cell(49,7.5,"",'R',0,'L',true);
-        $this->Cell(30,7.5,"",'R',0,'L',true);
-        $this->Cell(20,7.5,"",'R',0,'L',true);
-        $this->Cell(29,7.5,"",'',0,'L',true);
-        $this->SetXY(71,4+$y+1);
+        $this->SetXY(71,7+$y);
+        $this->Cell(49,10,"",'R',0,'L',true);
+        $this->Cell(30,10,"",'R',0,'L',true);
+        $this->Cell(20,10,"",'R',0,'L',true);
+        $this->Cell(29,10,"",'',0,'L',true);
+        $this->SetXY(71,7+$y+1);
         $this->SetFont('Arial','I',8); // italic 8px
         $this->Cell(49,2.5,"Faltas",0,0,'L',false);
         $this->Cell(30,2.5,"Rehuses",0,0,'L',false);
@@ -168,26 +183,27 @@ class EquiposByJornada extends PrintCommon {
 
         // take care on RFEC contests
         if ($this->federation->getFederation()==1) {
-            $this->pos[1]-=2; $this->pos[2]-=3; $this->pos[3]+=20; $this->pos[8]-=15;
+            $this->pos[1] -= 2;
+            $this->pos[2] -= 3;
+            $this->pos[3] += 20;
+            $this->pos[8] -= 15;
         }
-        $order=0;
+        $index=0;
         $rowcount=0;
+        $this->categoria="-";
 		foreach($this->equipos as $equipo) {
             $miembros=$equipo['Perros'];
             $num=count($miembros);
             if ($num==0) continue; // skip empty teams
-            if ($rowcount%10==0) {
+            if ( ($rowcount%10==0) || ($equipo['Categorias']!=$this->categoria)) {
+                $rowcount=0;
+                $this->categoria=$equipo['Categorias'];
                 $this->AddPage();
-                // indicamos nombre del operador que rellena la hoja
-                $this->ac_header(2,12);
-                $this->Cell(90,7,'Apunta:','LTBR',0,'L',true);
-                $this->Cell(10,7,'',0,'L',false);
-                $this->Cell(90,7,'Revisa:','LTBR',0,'L',true);
-                $this->Ln(8);
             }
             // pintamos el aspecto general de la celda
-            $this->printTeamInfo($rowcount,$equipo,$miembros);
+            $this->printTeamInfo($rowcount,$index,$equipo,$miembros);
             $rowcount++;
+            $index++;
 		}
 		// Línea de cierre
 		$this->myLogger->leave();
@@ -198,11 +214,12 @@ class EquiposByJornada extends PrintCommon {
 try {
 	$prueba=http_request("Prueba","i",0);
 	$jornada=http_request("Jornada","i",0);
-	// 	Creamos generador de documento
-	$pdf = new EquiposByJornada($prueba,$jornada);
+    $manga=http_request("Manga","i",0);
+    // 	Creamos generador de documento
+    $pdf=new EntradaDeDatosEquipos4($prueba,$jornada,$manga);
 	$pdf->AliasNbPages();
 	$pdf->composeTable();
-	$pdf->Output("equiposByJornada.pdf","D"); // "D" means open download dialog
+	$pdf->Output("entradaDeDatosEquipos4.pdf","D"); // "D" means open download dialog
 } catch (Exception $e) {
 	die ("Error accessing database: ".$e->getMessage());
 };
