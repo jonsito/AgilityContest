c91495090ec9f29e2610b7484663785c1b07abd3
jonsito <jonsito@gmail.com>
2015-08-04 18:18:07 +0200

    Update README_XAMPP. New Launcher AgilityContest.bat

---
+++ AgilityContest.bat
@@ -0,0 +1,33 @@
+@echo off
+cd /d %~dp0\..\xampp
+echo AgilityContest Launch Script
+echo ----------------------------------------
+echo NO CERRAR ESTA VENTANA DURANTE LA SESION
+echo ----------------------------------------
+echo MySQL Database is trying to start
+echo Please wait  ...
+
+echo Starting MySQL Database server...
+start /B "" \xampp\mysql\bin\mysqld --defaults-file=mysql\bin\my.ini --standalone --console
+echo Starting Apache Web Server....
+start /B "" \xampp\apache\bin\httpd.exe
+
+echo Opening AgilityContest console...
+start /W /MAX "AgilityContest" https://localhost/agility/console
+
+pause Press enter to finish AgilityContest session
+
+echo Apache Web Server shutdowm ...
+\xampp\apache\bin\pv -f -k httpd.exe -q
+if not exist apache\logs\httpd.pid GOTO stop_mysql
+del apache\logs\httpd.pid
+
+:stop_mysql
+echo MySQL DataBase shutdowm ...
+\xampp\apache\bin\pv -f -k mysqld.exe -q
+
+if not exist mysql\data\%computername%.pid GOTO finish
+echo Delete %computername%.pid ...
+del mysql\data\%computername%.pid
+
+:finish
