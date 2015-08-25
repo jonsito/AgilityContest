056552b4c28a1c3b06d4d7430c1e87c83738472d
jonsito <jonsito@gmail.com>
2015-08-26 00:23:36 +0200

    RaspChrono.py: first try

--- agility/chrono/RaspChrono.py
+++ agility/chrono/RaspChrono.py
@@ -1,48 +1,54 @@
-#!/usr/bin/env python
-#Imports for Pins,input
-http://raspberrypi.stackexchange.com/questions/19668/from-python-script-to-kernel-module
-import RPi.GPIO as GPIO
-import uinput
-from time import sleep
-
-#Setup
-key_events=( uinput.KEY_ESC, )
-device=uinput.Device(key_events)
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(17, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-
-#MAIN
-while True:
-   GPIO.wait_for_edge(17,GPIO.FALLING)
-   device.emit(uinput.KEY_ESC,1)
-   sleep(2)
-   device.emit(uinput.KEY_ESC,0)
-
-####################################################################
 #!/usr/bin/python3
-#gpiokeys.pi
+#######################################################################
+# monitorize GPIO pins and translate state changes into keyboard events
+#
+# Based in gpiokeys.py from "Raspberry PI CookBook for python programmers"
+# Notice: uinput kernel module must be modprobe'd before calling this program
+#
 import time
 import RPi.GPIO as GPIO
 import uinput
 
 #HARDWARE SETUP
-# P1
-# 2 [==G=====<=V==]26
-# 1 [===2=2>^=====]25
-B_DOWN  = 22 #V
-B_LEFT  = 18 #<
-B_UP    = 15 #^
-B_RIGHT = 13 #>
-B_1     = 11 #1
-B_2     = 7  #2
+
+# AgilityContest chrono mapping keys from chrono.js
+### Course learning
+# we can use any of these keys to start/stop course learning
+# KEY_0 : Course learning end
+# KEY_7 : Course learning start ( 7 minutes )
+
+#### Send data to Agility Contest
+# Reserved, but not used: as 2.0 version, these function should be performed from tablet.
+# KEY_F : Fault
+# KEY_R : Refusal
+# KEY_T : Touch
+# KEY_E : Eliminated
+# KEY_N : Not Present
+
+#### Chrono related functions
+# KEY_BACKSPACE : Reset chrono
+# KEY_HOME  : Start chrono count
+# KEY_END   : End chrono
+# KEY_PAUSE : Mark Intermediate time
+# KEY_ENTER : Start/Stop
+# KEY_S     : Alternate Start/Stop
+
+##### GPIO PIN Assignment
+# as stated above, data related events are not generated, and have no assigned pin
+GP_0     =  7   # BCM 4
+GP_7     =  11  # BCM 17
+GP_Reset =  13  # BCM 27
+GP_Start =  15  # BCM 22
+GP_Stop =   18  # BCM 24
+GP_Int =    22  # BCM 25
 
 DEBUG=True
-BTN = [B_UP,B_DOWN,B_LEFT,B_RIGHT,B_1,B_2]
-MSG = ["UP","DOWN","LEFT","RIGHT","1","2"]
+BTN = [ GP_0,   GP_7,   GP_Reset,   GP_Start,   GP_Stop,    GP_Int ]
+MSG = [ "BeginRec","EndRec","Reset","Start","Stop","Intermediate"]
 
 #Setup the DPad module pins and pull-ups
-def dpad_setup():
-	# set up the wiring
+def ac_gpio_setup():
+	# set up the wiring by pin socket number instead of BroadCom pinout number
 	GPIO.setmode(GPIO.BOARD)
 	# set BTN ports as INPUTS
 	for val in BTN:
@@ -51,12 +57,12 @@ def dpad_setup():
 		GPIO.setup(val, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 def main():
-	# Setup uinput
-	events = ( uinput.KEY_UP,uinput.KEY_DOWN,uinput.KEY_LEFT,uinput.KEY_RIGHT,uinput.KEY_ENTER,uinput.KEY_ENTER )
+	# Setup uinput. Notice that Start and Stop pins are mapped to same (Start/Stop) Key, to allow reversal of first/last jump
+	events = ( uinput.KEY_0,    uinput.KEY_7,   uinput.KEY_BACKSPACE,   uinput.KEY_ENTER,   uinput.KEY_ENTER,   uinput.KEY_PAUSE )
 	device = uinput.Device(events)
-	time.sleep(2) # seconds
-	dpad_setup()
-	print("DPad Ready!")
+	# let the kernel to take enought time to create user input device and initialize
+	time.sleep(3) # seconds
+	ac_gpio_setup()
 	btn_state = [False,False,False,False,False,False]
 	key_state = [False,False,False,False,False,False]
 	while True:
@@ -70,82 +76,20 @@ def main():
 		# Perform the button presses/releases,( but only change state once )
 		for idx, val in enumerate(btn_state):
 			if val == True and key_state[idx] == False:
-				if DEBUG: print (str(val) + ":" + MSG[idx])
+				if DEBUG: print (str(val) + ":" + MSG[idx] + " Press")
 				device.emit(events[idx],1) # press
 				key_state[idx]=True
 			elif val == False and key_state[idx] == True:
-				if DEBUG: print (str(val) + ":!" + MSG[idx])
+				if DEBUG: print (str(val) + ":" + MSG[idx] + " Release")
 				device.emit(events[idx],0) # release
 				key_state[idx]=False
 
-		time.sleep(.1)
+        # Note that due to next line time precission becomes to cents of second
+        # So need to rewrite this code in an event driven way
+		time.sleep(.01) # enought to bypass bounce buttons
 
 try:
 	main()
 finally:
 	GPIO.cleanup()
 # End
-
-#################################################################
-https://www.raspberrypi.org/forums/viewtopic.php?t=43216&p=346413
-    #!/usr/bin/env python
-
-
-    ### IMPORTS ###
-    import os                           # Allows us to run console commands and programs
-    from time import sleep                        # Allows us to call the sleep function to slow down our loop
-    import datetime                           # Allows us to look at the time
-    import RPi.GPIO as GPIO                        # Allows us to call our GPIO pins and names it just GPIO
-    GPIO.setmode(GPIO.BCM)                        # Set's GPIO pins to BCM GPIO numbering
-
-    b1count = 1
-    b2count = 1
-    b3count = 1
-
-    ### GPIO SETUP ###
-    GPIO.setup(23, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)            # Set input pin to use the internal pull down resistor
-    GPIO.setup(24, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)            # Set input pin to use the internal pull down resistor
-    GPIO.setup(25, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)            # Set input pin to use the internal pull down resistor
-
-
-    ### BUTTON PRESS FUNCTIONS ###
-    def Input_1(channel):
-        os.system('xte "key q"')                     # Xte converting button Presses into Keystrokes.
-        global b1count                        # Imports global variable set above
-        now = datetime.datetime.now()                  # What time is it now?
-        print ' - Button 1 - %r - %r' % (b1count, now.strftime("%H:%M:%S.%f"))   # Prints which button was pressed, how many times, and timestamps it
-        b1count = b1count+1                        # Increments button count
-
-
-    def Input_2(channel):
-        os.system('xte "key h"')                     # Xte converting button Presses into Keystrokes.
-        global b2count                        # Imports global variable set above
-        now = datetime.datetime.now()                  # What time is it now?
-        print ' - Button 2 - %r - %r' % (b2count, now.strftime("%H:%M:%S.%f"))   # Prints which button was pressed, how many times, and timestamps it
-        b2count = b2count+1                        # Increments button count
-
-    def Input_3(channel):
-        os.system('xte "key v"')                     # Xte converting button Presses into Keystrokes.
-        global b3count                        # Imports global variable set above
-        now = datetime.datetime.now()                  # What time is it now?
-        print ' - Button 3 - %r - %r' % (b3count, now.strftime("%H:%M:%S.%f"))   # Prints which button was pressed, how many times, and timestamps it
-        b3count = b3count+1                        # Increments button count
-
-
-    ### BUTTON PRESS DETECTION ###
-    ### Does a Callback to the appropriate Input function.  Also debounces to prevent clicking the button multiple times a second.
-    GPIO.add_event_detect(23, GPIO.FALLING, callback=Input_1, bouncetime=1000)   # Waiting for Button 1 to be pressed.
-    GPIO.add_event_detect(24, GPIO.FALLING, callback=Input_2, bouncetime=1000)   # Waiting for Button 2 to be pressed.
-    GPIO.add_event_detect(25, GPIO.FALLING, callback=Input_3, bouncetime=1000)   # Waiting for Button 3 to be pressed.
-
-
-    ### FEH -D - Slideshow -Z - Zooms -Y Hides Pointer -F Fullscreen -r loads all images in the following directory###
-    os.system('feh -D 0.25 -Z -Y -F -r /home/pi/Desktop/Pics/')         # Runs Feh Image Viewer with some command line arguments.
-
-
-    ### Starts a neverending loop otherwise the script will just quit.
-    while True:
-        print "Waiting for input."                     # Insert Random Loop Junk
-        sleep(60);                           # Sleeps for a minute to save CPU cycles.  Any interrupt will break this.
-
-    GPIO.cleanup()
