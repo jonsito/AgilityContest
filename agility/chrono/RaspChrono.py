31adb33603f12c4c6083f5d5a0b652915761a179
jonsito <jonsito@gmail.com>
2015-08-26 00:55:47 +0200

    RaspChrono.py: second try. now event driven

--- agility/chrono/RaspChrono.py
+++ agility/chrono/RaspChrono.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 #######################################################################
-# monitorize GPIO pins and translate state changes into keyboard events
+# Monitorize Raspberry PI GPIO pins and translate state changes into
+# AgilityContest Chrono keyboard event keys
 #
 # Based in gpiokeys.py from "Raspberry PI CookBook for python programmers"
+# and code from http://raspberrywebserver.com/gpio/using-interrupt-driven-gpio.html
+#
 # Notice: uinput kernel module must be modprobe'd before calling this program
 #
 import time
@@ -35,21 +38,48 @@ import uinput
 
 ##### GPIO PIN Assignment
 # as stated above, data related events are not generated, and have no assigned pin
-GP_0     =  7   # BCM 4
+GP_0     =  7   # BCM 04
 GP_7     =  11  # BCM 17
 GP_Reset =  13  # BCM 27
 GP_Start =  15  # BCM 22
-GP_Stop =   18  # BCM 24
-GP_Int =    22  # BCM 25
+GP_Stop  =  18  # BCM 24
+GP_Int   =  22  # BCM 25
+GP_LED   =  26  # BCM 07
 
 DEBUG=True
 BTN = [ GP_0,   GP_7,   GP_Reset,   GP_Start,   GP_Stop,    GP_Int ]
 MSG = [ "BeginRec","EndRec","Reset","Start","Stop","Intermediate"]
 
+# current button on/off and key press/release status
+btn_state = [False,False,False,False,False,False]
+key_state = [False,False,False,False,False,False]
+
+# Event handler. By default, on interrupt poll state on all buttons
+def event_handler(pin):
+	# Catch all the buttons pressed before pressing related keys
+	for idx, val in enumerate(BTN):
+		if GPIO.input(val) == False:
+			btn_state[idx]=True
+		else:
+			btn_state[idx]=False
+
+	# Perform the button presses/releases,( but only change state once )
+	for idx, val in enumerate(btn_state):
+		if val == True and key_state[idx] == False:
+			if DEBUG: print (str(val) + ":" + MSG[idx] + " Press")
+			device.emit(events[idx],1) # press
+			key_state[idx]=True
+		elif val == False and key_state[idx] == True:
+			if DEBUG: print (str(val) + ":" + MSG[idx] + " Release")
+			device.emit(events[idx],0) # release
+			key_state[idx]=False
+
 #Setup the DPad module pins and pull-ups
 def ac_gpio_setup():
 	# set up the wiring by pin socket number instead of BroadCom pinout number
 	GPIO.setmode(GPIO.BOARD)
+	# Set GP_LED as output
+	GPIO.setup(GP_LED, GPIO.OUT)
 	# set BTN ports as INPUTS
 	for val in BTN:
 		# Set up GPIO input with pull up control
@@ -63,30 +93,21 @@ def main():
 	# let the kernel to take enought time to create user input device and initialize
 	time.sleep(3) # seconds
 	ac_gpio_setup()
-	btn_state = [False,False,False,False,False,False]
-	key_state = [False,False,False,False,False,False]
+
+	# listen for events and share callback. Use 100ms for button bounce time
+	GPIO.add_event_detect(GP_0,     GPIO.BOTH, callback=event_handler, bouncetime=100)
+	GPIO.add_event_detect(GP_7,     GPIO.BOTH, callback=event_handler, bouncetime=100)
+	GPIO.add_event_detect(GP_Reset, GPIO.BOTH, callback=event_handler, bouncetime=100)
+	GPIO.add_event_detect(GP_Start, GPIO.BOTH, callback=event_handler, bouncetime=100)
+	GPIO.add_event_detect(GP_Stop,  GPIO.BOTH, callback=event_handler, bouncetime=100)
+	GPIO.add_event_detect(GP_Int,   GPIO.BOTH, callback=event_handler, bouncetime=100)
+
+	# and enter into infinite loop setting led on/off
 	while True:
-		# Catch all the buttons pressed before pressing related keys
-		for idx, val in enumerate(BTN):
-			if GPIO.input(val) == False:
-				btn_state[idx]=True
-			else:
-				btn_state[idx]=False
-
-		# Perform the button presses/releases,( but only change state once )
-		for idx, val in enumerate(btn_state):
-			if val == True and key_state[idx] == False:
-				if DEBUG: print (str(val) + ":" + MSG[idx] + " Press")
-				device.emit(events[idx],1) # press
-				key_state[idx]=True
-			elif val == False and key_state[idx] == True:
-				if DEBUG: print (str(val) + ":" + MSG[idx] + " Release")
-				device.emit(events[idx],0) # release
-				key_state[idx]=False
-
-        # Note that due to next line time precission becomes to cents of second
-        # So need to rewrite this code in an event driven way
-		time.sleep(.01) # enought to bypass bounce buttons
+	    GPIO.output(GP_LED,True)
+        time.sleep(1)
+        GPIO.output(GP_LED,False)
+        time.sleep(1)
 
 try:
 	main()
