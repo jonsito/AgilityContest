b27791bfa356de626635b5604428a8e3b7fd2873
Juan Antonio Martinez <jonsito@gmail.com>
2015-08-28 15:12:52 +0200

    more on gpio to json on raspberry

--- agility/chrono/RaspChrono_events.py
+++ agility/chrono/RaspChrono_events.py
@@ -9,10 +9,30 @@
 # Notice: uinput kernel module must be modprobe'd before calling this program
 #
 import time
+import json
+import urllib2
 import RPi.GPIO as GPIO
 import uinput
 
-#HARDWARE SETUP
+from datetime import datetime
+from datetime import timedelta
+
+##### GPIO PIN Assignment
+
+# Chrono action buttons
+GP_0     =   3  # GPIO 02
+GP_7     =   5  # GPIO 03
+GP_Reset =   7  # GPIO 04
+GP_Start =  11  # GPIO 17
+GP_Stop  =  13  # GPIO 27
+GP_Int   =  15  # GPIO 22
+
+# Session ID 00..11 -> 1st, 2nd, 3rd, 4th available Ring session ( default 2) TODO: to be discovered
+GP_SES0  =  22  # GPIO 25
+GP_SES1  =  24  # GPIO 08
+
+# Output led
+GP_LED   =  26  # GPIO 07
 
 # AgilityContest chrono json request based sensor monitor
 #Request to server are made by sending json request to:
@@ -31,26 +51,32 @@ import uinput
 # Session= Session ID to join. "2" means normally ring 1
 # Source= Chronometer ID. should be in form "chrono_sessid"
 # Value= Timestamp. Number of milliseconds since chrono started to run
+# Timestamp= Timestamp. same value as "Value" ( obsoleted, but still needed )
 #
-# ?Operation=chronoEvent&Type=crono_rec&TimeStamp=150936&Source=chrono_2&Session=2&Prueba=11&Jornada=81&Manga=19&Tanda=49&Value=150936
+# ?Operation=chronoEvent&Type=crono_rec&TimeStamp=150936&Source=chrono_2&Session=2&Value=150936
+# data = json.load( urllib2.urlopen('http://ip.addr.of.server/agility/server/database/eventFunctions.php') + arguments )
 
-##### GPIO PIN Assignment
-# as stated above, data related events are not generated, and have no assigned pin
-GP_0     =  7   # BCM 04
-GP_7     =  11  # BCM 17
-GP_Reset =  13  # BCM 27
-GP_Start =  15  # BCM 22
-GP_Stop  =  18  # BCM 24
-GP_Int   =  22  # BCM 25
-GP_LED   =  26  # BCM 07
-
+##### default server config
+SERVER = "192.168.122.1"	# TODO: auto detect
+SESSION_ID = 2				# TODO: read from GPIO Switches
+SESSION_NAME = "Chrono_2"	# should be generated from evaluated session ID
 DEBUG=True
+
+# buttons array and their names
 BTN = [ GP_0,   GP_7,   GP_Reset,   GP_Start,   GP_Stop,    GP_Int ]
 MSG = [ "BeginRec","EndRec","Reset","Start","Stop","Intermediate"]
-
 # current button on/off and key press/release status
 btn_state = [False,False,False,False,False,False]
 key_state = [False,False,False,False,False,False]
+
+# store datetime from program start
+start_time = datetime.now()
+
+# returns the elapsed milliseconds since the start of the program
+def millis():
+   dt = datetime.now() - start_time
+   ms = (dt.days * 24 * 60 * 60 + dt.seconds) * 1000 + dt.microseconds / 1000.0
+   return ms
 
 # Event handler. By default, on interrupt poll state on all buttons
 def event_handler(pin):
@@ -65,15 +91,20 @@ def event_handler(pin):
 		else:
 			btn_state[idx]=False
 
+	timestamp= millis()
 	# Perform the button presses/releases,( but only change state once )
 	for idx, val in enumerate(btn_state):
 		if val == True and key_state[idx] == False:
 			if DEBUG: print (str(val) + ":" + MSG[idx] + " Press")
-			device.emit(events[idx],1) # press
+			# compose json request
+			args = ?Operation=chronoEvent&Type=crono_rec&TimeStamp=150936&Source=chrono_2&Session=2&Value=150936
+			url="http://"+SERVER+"/agility/server/database/eventFunctions.php"
+			# send request . It is safe to ignore response
+			json.load( urllib2.urlopen( url + args ) )
 			key_state[idx]=True
 		elif val == False and key_state[idx] == True:
 			if DEBUG: print (str(val) + ":" + MSG[idx] + " Release")
-			device.emit(events[idx],0) # release
+			# no action needed on release
 			key_state[idx]=False
 
 #Setup the DPad module pins and pull-ups
@@ -99,12 +130,12 @@ def main():
 	ac_gpio_setup()
 
 	# listen for events and share callback. Use 1s for button bounce time
-	GPIO.add_event_detect(GP_0,     GPIO.BOTH, callback=event_handler, bouncetime=1000)
-	GPIO.add_event_detect(GP_7,     GPIO.BOTH, callback=event_handler, bouncetime=1000)
-	GPIO.add_event_detect(GP_Reset, GPIO.BOTH, callback=event_handler, bouncetime=1000)
-	GPIO.add_event_detect(GP_Start, GPIO.BOTH, callback=event_handler, bouncetime=1000)
-	GPIO.add_event_detect(GP_Stop,  GPIO.BOTH, callback=event_handler, bouncetime=1000)
-	GPIO.add_event_detect(GP_Int,   GPIO.BOTH, callback=event_handler, bouncetime=1000)
+	GPIO.add_event_detect(GP_0,     GPIO.FALLING, callback=event_handler, bouncetime=1000)
+	GPIO.add_event_detect(GP_7,     GPIO.FALLING, callback=event_handler, bouncetime=1000)
+	GPIO.add_event_detect(GP_Reset, GPIO.FALLING, callback=event_handler, bouncetime=1000)
+	GPIO.add_event_detect(GP_Start, GPIO.FALLING, callback=event_handler, bouncetime=1000)
+	GPIO.add_event_detect(GP_Stop,  GPIO.FALLING, callback=event_handler, bouncetime=1000)
+	GPIO.add_event_detect(GP_Int,   GPIO.FALLING, callback=event_handler, bouncetime=1000)
 
 	# and enter into infinite loop setting led on/off
 	count = 0
