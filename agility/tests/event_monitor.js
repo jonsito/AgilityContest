52f4c46eda7251400a9fc40c04a4faea3d424db4
jonsito <jonsito@gmail.com>
2016-05-07 20:51:35 +0200

    event_monitor.js: AgilityContest server discovery now works fine.

--- agility/tests/event_monitor.js
+++ agility/tests/event_monitor.js
@@ -19,16 +19,24 @@ if not, write to the Free Software Foundation, Inc., 51 Franklin Street, Fifth F
 * browser-less implementation of AgilityContest event protocol parser
 *
 * Requires nodejs >=4
-* Required npm modules http querystring os ip
+* Required npm modules http sync-request querystring os ip
+*
 */
 /***************************************************************************************************************/
 var querystring = require('querystring');
 var http = require('http');
 
 var workingData = {
-	// hostname and sessionID.
+	// Comment as desired
+	//************ Option 1- when server hostname is not set you must provide ring name to search for
+	ring: 1, // must be set by user in range (1..4)
 	hostname: '0.0.0.0', // set to '0.0.0.0' to let the app find server by itself
-	sessionID: 2,
+	sesionID: 0,
+	//************* Option 2 we already know hostname and sessionID
+	// ring: 0, // ignored as implicit in sessionID
+	// hostname: 'localhost',
+	// sessionID: 2,
+
 	// variables to store start timestamp mark
 	cronomanual: 0,
 	cronoauto: 0,
@@ -157,8 +165,8 @@ var eventHandler= {
 /**
  * Generic event handler browser-less event monitor
  * Every screen has a 'eventHandler' table with pointer to functions to be called
- * @param id {integer} Event ID
- * @param evt {array} Event data
+ * @param {int} id Event ID
+ * @param {array} event Event data
  */
 function event_parser(id,event) {
 	event['ID']=id; // fix real id on stored eventData
@@ -173,9 +181,10 @@ function event_parser(id,event) {
  * Notice that due to async nature of http requests, a dirty trick is needed
  * to wait for host polling to finish. this should be revisited in a later revision
  *
+ * @param {int} ring ring number to search their sessionID for
  * @returns {boolean} true when server and session found. otherwise false
  */
-function findServer() {
+function findServer(ring) {
 	var addresses = []; // to be evaluated
 
 	// locate any non local interface ipaddress/mask
@@ -200,38 +209,27 @@ function findServer() {
 	 * @return true on success; otherwise false
      */
 	function connectServer(hostaddr){
-
-		var postData =querystring.stringify({
-			'Operation' : 'selectring'
-		});
-		var options= {
-			protocol: 'http:',
-			hostname: hostaddr,
-			port: 80,
-			path: '/agility/server/database/sessionFunctions.php',
-			method: 'POST',
-			headers: {
-				'Content-Type': 'application/x-www-form-urlencoded',
-				'Content-Length': postData.length
-			}
-		};
-
-		function handleSuccess(data) {
-			console.log("Received response at host:"+hostaddr);
+		var url="http://"+hostaddr+"/agility/server/database/sessionFunctions.php?Operation=selectring";
+		var request = require('sync-request');
+		try {
+			var res = request('GET', url, {
+				timeout: 250,
+				headers: { // this is for some routers that return "Auth required" to fail and send proper error code
+					authorization: 'Basic ' + new Buffer('AgilityContest' + ':' + 'AgilityContest', 'ascii').toString('base64')
+				}
+			});
+			if (res.statusCode!=200) return false; // http request failed
+			console.log("Found AgilityContest server at: "+hostaddr);
+			var data = JSON.parse(res.getBody('utf8'));
+			// this code assumes that first returned row matches ring 1, second ring 2 and so
+			workingData.hostname=hostaddr;
+			workingData.sessionID=parseInt(data['rows'][workingData.ring-1]['ID']);
+			console.log("SessionID for ring:"+workingData.ring+" is:"+workingData.sessionID);
+			return true;
+		} catch (err) {
+			console.log("Host: "+hostname+" Error: "+err);
+			return false;
 		}
-		function handleError(data) {
-            console.log("Error at host:"+hostaddr);
-		}
-		console.log("Looking for server at:"+hostaddr);
-		var req = http.request(options,function(res){
-			res.setEncoding('utf8');
-			res.on('data', handleSuccess);
-			res.on('end', function() { /* empty */  });
-		});
-		req.on('error',handleError);
-		// write data to request body
-		req.write(postData);
-		req.end();
 	}
 
 	getInterfaces(); // retrieve network interface information
@@ -317,6 +315,7 @@ function waitForEvents(evtID,timestamp){
  * On sucess invoke waitForEvents and enter in event parsing loop
  */
 function startEventMgr() {
+
     var postData = querystring.stringify({
         'Operation':'connect',
         'Session': workingData.sessionID
@@ -347,6 +346,7 @@ function startEventMgr() {
             if ( parseInt(response['total'])!=0) {
                 var row=response['rows'][0];
                 var evtID=row['ID'];
+				console.log("Connected to AgilityContest Event bus");
                 setTimeout(function(){ waitForEvents(evtID,0);},0);
             } else {
                 setTimeout(function(){ startEventMgr(); },timeout );
@@ -372,4 +372,4 @@ if (workingData.hostname==='0.0.0.0') {
 	if (!findServer()) process.exit(1);
 }
 // start event manager
-startEventMgr(); // use hard-wired session ID 2 ( ring 1 )
+startEventMgr();
