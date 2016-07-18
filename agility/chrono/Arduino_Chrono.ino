bd5314b1638f10859dd154f32dba6cad3d8998c2
jonsito <jonsito@gmail.com>
2016-07-18 20:11:32 +0200

    arduino: handle start/stop/intermediate buttons. Fix rebounce on button release

--- agility/chrono/Arduino_Chrono.ino
+++ agility/chrono/Arduino_Chrono.ino
@@ -33,49 +33,83 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 TM1638 module(8, 9, 7);
 
 volatile int running; // to control chrono state
-volatile unsigned long isrTime; // to control debouncing
+volatile int paused;
 unsigned long startTime; // timestamp marks for start and stop (millis)
 unsigned long stopTime;
+unsigned long pauseTime;
 
 int ledPin=13; // internal led
+int irqPins[]={2,3,1};  // start/stop/intermediate input pins to trigger irq
+unsigned long isrTime[]={0,0,0}; // time of each pin interrupt
 
 void setup() {
+  int n;
   // set up led
   pinMode(ledPin,OUTPUT);
-  pinMode(2,INPUT);
-  digitalWrite(2,HIGH); // input, pull up
+  
   // set up timer
   running=0;
+  paused=0;
   startTime=0;
   stopTime=0;
-  isrTime=millis();
-  // catch interrupt
-  attachInterrupt(digitalPinToInterrupt(2), isr, FALLING);
+  // catch interrupt from input lines
+  pinMode(irqPins[0],INPUT_PULLUP);
+  pinMode(irqPins[1],INPUT_PULLUP);
+  pinMode(irqPins[2],INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(irqPins[0]), startIRQ, FALLING);
+  attachInterrupt(digitalPinToInterrupt(irqPins[1]), stopIRQ, FALLING);
+  attachInterrupt(digitalPinToInterrupt(irqPins[2]), intermediateIRQ, FALLING);
+
   // and say we are ready
   module.setDisplayToString(" HELLO ",0x00);
   delay(5000); // time to see message :-)
 }
 
-void startChrono(long timestamp) { startTime=timestamp; running=true; }
-void stopChrono(long timestamp) { stopTime=timestamp; running=false; }
-
-void isr() { // interrupt service routine
-  unsigned long tstamp=millis();
-  if ( (tstamp-isrTime)<50 ) return; // debouncing time is active 
-  isrTime=tstamp;
-  if (running) stopChrono(tstamp);
-  else startChrono(tstamp);
+void startChrono(unsigned long timestamp) { startTime=timestamp; running=true; }
+void stopChrono(unsigned long timestamp) { 
+  if ((timestamp-startTime)<2000) return; // 2 seconds safeguard to stop 
+  stopTime=timestamp; 
+  running=false;
 }
+void pauseChrono(unsigned long timestamp) { pauseTime=timestamp; paused=true; }
+
+void handleIRQ(int n) { // interrupt service routine
+  unsigned long tstamp=millis();
+  if ( (tstamp-isrTime[n])<50 ) return; // debouncing time is active
+  isrTime[n]=tstamp;
+  // ok: no more bounces at pin 'n': check state. if high means low-to-high: so ignore
+  int state=digitalRead(irqPins[n]);
+  if (state==HIGH) return; // button release: ignore
+  if (running && (n==2)) pauseChrono(tstamp);
+  else {
+    if (running) stopChrono(tstamp);
+    else startChrono(tstamp);
+  }
+}
+
+void startIRQ () { handleIRQ(0); }
+void stopIRQ () { handleIRQ(1); }
+void intermediateIRQ () { handleIRQ(2); }
 
 void loop() {
   char display[8];
   unsigned long currentTime=millis(); // capture timestamp
-  unsigned long ellapsed=currentTime-startTime;
+  unsigned long ellapsed;
+  
   // mark running status
   digitalWrite(ledPin,running);
+
+  // on stopped, evaluate time to show
+  if (!running && !paused) ellapsed=0;
+  if (!running && paused ) ellapsed=0;
+  if (running && !paused ) ellapsed=currentTime-startTime;
+  if (running && paused ) ellapsed=pauseTime-startTime;
   if (!running) ellapsed=stopTime-startTime;
-  module.setDisplayToDecNumber(ellapsed/10,0x04 |(running<<8),true);
+  if ((pauseTime-currentTime)>4000) paused=false; // restart from pause after 5 seconds
+  module.setDisplayToDecNumber(ellapsed/10,0x04,true);
+  
   // pull i2c buttons
   module.setLEDs(module.getButtons());
   delay(50-(millis()-currentTime));
 }
+
