0f29d25725ec2d00f54a27eca415b8f8ac3c2166
jonsito <jonsito@gmail.com>
2017-04-10 23:05:53 +0200

    Some fixes to .bat and .exe launcher

--- extras/AgilityContest.bat
+++ extras/AgilityContest.bat
@@ -1,7 +1,7 @@
 @echo off
 call settings.bat
 cd /d %~dp0\xampp
-echo AgilityContest Launch Script
+rem echo AgilityContest Launch Script
 
 rem notice that this may require admin privileges
 rem for windows 8 and 10 disable w3svc service
@@ -13,27 +13,29 @@ netsh advfirewall firewall add rule name="Apache HTTPs Server" action=allow prot
 
 rem if required prepare portable xampp to properly setup directories
 if not exist ..\logs\first_install GOTO mysql_start
-echo Configuring first boot of XAMPP
+rem echo Configuring first boot of XAMPP
 set PHP_BIN=php\php.exe
 set CONFIG_PHP=install\install.php
-%PHP_BIN% -n -d output_buffering=0 -q %CONFIG_PHP% usb
+%PHP_BIN% -n -d output_buffering=0 -q %CONFIG_PHP% usb >nul
 
 rem start mysql database server
 :mysql_start
-echo MySQL Database is trying to start
-echo Please wait  ....
-start /B "" mysql\bin\mysqld --defaults-file=mysql\bin\my.ini --standalone --console
-timeout /t 5
+rem echo MySQL Database is trying to start
+rem echo Please wait  ....
+start "" /B mysql\bin\mysqld --defaults-file=mysql\bin\my.ini --standalone --console >nul
+rem timeout  5
+ping -n 5 127.0.0.1 >nul
 
 rem start apache web server
 :apache_start
-echo Starting Apache Web Server....
-start /B "" apache\bin\httpd.exe
-timeout /t 5
+rem echo Starting Apache Web Server....
+start "" /B apache\bin\httpd.exe >nul
+rem timeout  5
+ping -n 5 127.0.0.1 >nul
 
 rem on first run create database and database users
 if not exist ..\logs\first_install GOTO browser_start
-echo Creating AgilityContest Databases. Please wait
+rem echo Creating AgilityContest Databases. Please wait
 timeout /t 5
 echo DROP DATABASE IF EXISTS agility; > ..\logs\install.sql
 echo CREATE DATABASE agility; >> ..\logs\install.sql
@@ -43,14 +45,14 @@ type ..\extras\users.sql >> ..\logs\install.sql
 mysql\bin\mysql -u root < ..\logs\install.sql
 del ..\logs\install.sql
 del ..\logs\first_install
-echo Opening AgilityContest console for first time...
-start /WAIT /MAX "AgilityContest" https://localhost/agility/console/index.php?installdb=1
+rem echo Opening AgilityContest console for first time...
+start /MAX "AgilityContest" https://localhost/agility/console/index.php?installdb=1
 goto wait_for_end
 
 rem normal start when database is installed
 :browser_start
-echo Opening AgilityContest console...
-start /WAIT /MAX "AgilityContest" https://localhost/agility/console
+rem echo Opening AgilityContest console...
+start /MAX "AgilityContest" https://localhost/agility/console
 
 :wait_for_end
 exit
