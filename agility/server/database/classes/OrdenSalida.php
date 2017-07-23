0a40ac4b0c6aadf40392550fe7b94642aa45aadb
jonsito <jonsito@gmail.com>
2017-07-23 03:03:55 +0200

    Resultados-OrdenSalida: trying to fix circular require_once.try 2

--- agility/server/database/classes/OrdenSalida.php
+++ agility/server/database/classes/OrdenSalida.php
@@ -17,6 +17,7 @@ if not, write to the Free Software Foundation, Inc., 51 Franklin Street, Fifth F
 */
 
 require_once(__DIR__."/../../../modules/Federations.php");
+require_once(__DIR__."/../../../modules/Competitions.php");
 require_once("DBObject.php");
 require_once("Equipos.php");
 require_once("Resultados.php");
