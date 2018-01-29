a55dece7a3b08a78d574ef2b5e1e48e9828cb218
Juan Antonio Martinez <jonsito@gmail.com>
2018-01-29 10:47:26 +0100

    Ligas: add C? to store round califications
    Will be used to show individual (per dog) results

--- agility/server/database/classes/Ligas.php
+++ agility/server/database/classes/Ligas.php
@@ -61,9 +61,10 @@ class Ligas extends DBObject {
         // create prepared statement
         $sql="INSERT INTO Ligas (".
             "Jornada,Grado,Perro,".
-            "Pt1,Pt2,Pt3,Pt4,Pt5,Pt6,Pt7,Pt8,St1,St2,St3,St4,St5,St6,St7,St8,Xt1,Xt2,Xt3,Xt4,Xt5,Xt6,Xt7,Xt8,".
+            "Pt1,Pt2,Pt3,Pt4,Pt5,Pt6,Pt7,Pt8,St1,St2,St3,St4,St5,St6,St7,St8,".
+            "Xt1,Xt2,Xt3,Xt4,Xt5,Xt6,Xt7,Xt8,C1,C2,C3,C4,C5,C6,C7,C8,".
             "Puntos,Estrellas,Extras)".
-            " VALUES (?,?,?,  ?,?,?,?,?,?,?,?, ?,?,?,?,?,?,?,?, ?,?,?,?,?,?,?,?, ?,?,?)".
+            " VALUES (?,?,?,  ?,?,?,?,?,?,?,?, ?,?,?,?,?,?,?,?, ?,?,?,?,?,?,?,?, ?,?,?,?,?,?,?,?, ?,?,?)".
             " ON DUPLICATE KEY UPDATE ".
             " Pt1=VALUES(Pt1), Pt2=VALUES(Pt2), Pt3=VALUES(Pt3), Pt4=VALUES(Pt4), ".
             " Pt5=VALUES(Pt5), Pt6=VALUES(Pt6), Pt7=VALUES(Pt7), Pt8=VALUES(Pt8), ".
@@ -71,6 +72,8 @@ class Ligas extends DBObject {
             " St5=VALUES(St5), St6=VALUES(St6), St7=VALUES(St7), St8=VALUES(St8), ".
             " Xt1=VALUES(Xt1), Xt2=VALUES(Xt2), Xt3=VALUES(Xt3), Xt4=VALUES(Xt4), ".
             " Xt5=VALUES(Xt5), Xt6=VALUES(Xt6), Xt7=VALUES(Xt7), Xt8=VALUES(Xt8), ".
+            " C1=VALUES(C1), C2=VALUES(C2), C3=VALUES(C3), C4=VALUES(C4), ".
+            " C5=VALUES(C5), C6=VALUES(C6), C7=VALUES(C7), C8=VALUES(C8), ".
             "Puntos=VALUES(Puntos),Estrellas=VALUES(Estrellas),Extras=VALUES(Extras)";
         $stmt=$this->conn->prepare($sql);
         if (!$stmt) return $this->error($this->conn->error);
@@ -101,11 +104,12 @@ class Ligas extends DBObject {
                 $data['Extras']=array_key_exists("Extras",$item)?$item["Extras"]:0;
                 $this->myLogger->trace("LIGAS: Jornada:{$data['Jornada']},Grado:{$data['Grado']},Perro:{$data['Perro']}");
                 for ($n=1;$n<9;$n++) {
-                    $data["Pt{$n}"]= array_key_exists("Pt{$n}",$item)?$item["Pt{$n}"]:0;
-                    $data["St{$n}"]= array_key_exists("St{$n}",$item)?$item["St{$n}"]:0;
-                    $data["Xt{$n}"]= array_key_exists("Xt{$n}",$item)?$item["Xt{$n}"]:0;
+                    $data["Pt{$n}"]= array_key_exists("Pt{$n}",$item)?$item["Pt{$n}"]:0; // puntos
+                    $data["St{$n}"]= array_key_exists("St{$n}",$item)?$item["St{$n}"]:0; // estrellas
+                    $data["Xt{$n}"]= array_key_exists("Xt{$n}",$item)?$item["Xt{$n}"]:0; // extras
+                    $data["C{$n}"]= array_key_exists("C{$n}",$item)?$item["C{$n}"]:''; // calificaciones
                 }
-                $res=$stmt->bind_param("isiiiiiiiiiiiiiiiiiiiiiiiiiiii",
+                $res=$stmt->bind_param("isiiiiiiiiiiiiiiiiiiiiiiiiissssssssiii",
                     $data['Jornada'],$data['Grado'],$data['Perro'],
                     $data['Pt1'],$data['Pt2'],$data['Pt3'],$data['Pt4'],
                     $data['Pt5'],$data['Pt6'],$data['Pt7'],$data['Pt8'],
@@ -113,6 +117,8 @@ class Ligas extends DBObject {
                     $data['St5'],$data['St6'],$data['St7'],$data['St8'],
                     $data['Xt1'],$data['Xt2'],$data['Xt3'],$data['Xt4'],
                     $data['Xt5'],$data['Xt6'],$data['Xt7'],$data['Xt8'],
+                    $data['C1'],$data['C2'],$data['C3'],$data['C4'],
+                    $data['C5'],$data['C6'],$data['C7'],$data['C8'],
                     $data['Puntos'],$data['Estrellas'],$data['Extras']
                     );
                 if (!$res) return $this->error($stmt->error);
