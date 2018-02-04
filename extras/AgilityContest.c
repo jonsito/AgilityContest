66d44582f8385650a328e8e818f23cca01fe0202
jonsito <jonsito@gmail.com>
2018-02-04 20:15:11 +0100

    Launcher: AgilityContest.c done: start debugging
    Need to revise launchAndForget.

--- extras/AgilityContest.c
+++ extras/AgilityContest.c
@@ -19,7 +19,7 @@ int launchAndWait (char *cmd, char *args) {
     ZeroMemory(&StartupInfo, sizeof(StartupInfo));
     StartupInfo.cb = sizeof StartupInfo ; //Only compulsory field
 
-    if(CreateProcess(cmd, args, NULL,NULL,FALSE,0,NULL, NULL,&StartupInfo,&ProcessInfo))  {
+    if(CreateProcess(cmd, args, NULL,NULL,FALSE,CREATE_NO_WINDOW,NULL, NULL,&StartupInfo,&ProcessInfo))  {
         WaitForSingleObject(ProcessInfo.hProcess,INFINITE);
         CloseHandle(ProcessInfo.hThread);
         CloseHandle(ProcessInfo.hProcess);
@@ -31,8 +31,25 @@ int launchAndWait (char *cmd, char *args) {
 }
 
 int launchAndForget ( char *cmd, char *args) {
+    PROCESS_INFORMATION ProcessInfo; //This is what we get as an [out] parameter
+
+    STARTUPINFO StartupInfo; //This is an [in] parameter
+
+    ZeroMemory(&StartupInfo, sizeof(StartupInfo));
+    StartupInfo.cb = sizeof StartupInfo ; //Only compulsory field
+
+    if(! CreateProcess(cmd, args, NULL,NULL,FALSE,CREATE_NO_WINDOW,NULL, NULL,&StartupInfo,&ProcessInfo))  {
+        MessageBox (NULL, args,"failed", MB_OK | MB_ICONINFORMATION);
+    }
+    return 0;
 }
 
+int first_install() {
+    struct stat buffer;
+    int         status;
+    status = stat("..\\logs\\first_install",&buffer);
+    return (status==0)?1:0; // true on success
+}
 
 int WINAPI WinMain (HINSTANCE hInstance, HINSTANCE hPrevInst, LPTSTR lpCmdLine, int nShowCmd) {
 
@@ -40,7 +57,7 @@ int WINAPI WinMain (HINSTANCE hInstance, HINSTANCE hPrevInst, LPTSTR lpCmdLine, 
     char *msg="Hello World!";
     // call settings.bat
     // settings.bat sets default language. So just parse and setenv
-    FILE *f=fopen("./settings.bat","r");
+    FILE *f=fopen(".\\settings.bat","r");
     if (f) {
         char *str=calloc(32,sizeof(char));
         fgets(str,31,f);
@@ -83,55 +100,101 @@ int WINAPI WinMain (HINSTANCE hInstance, HINSTANCE hPrevInst, LPTSTR lpCmdLine, 
     args="netsh advfirewall firewall add rule name=\"Apache HTTPs Server\" action=allow protocol=TCP dir=in localport=443";
     launchAndWait(cmd,args);
 
-  return 0;
+    /* on first install set properly php environment (paths, configs and so ) */
+    if ( first_install() ) {
+        // rem if required prepare portable xampp to properly setup directories
+        // if not exist ..\logs\first_install GOTO mysql_start
+        // rem echo Configuring first boot of XAMPP
+        // set PHP_BIN=php\php.exe
+        // set CONFIG_PHP=install\install.php
+        // %PHP_BIN% -n -d output_buffering=0 -q %CONFIG_PHP% usb >nul
+        char *php=calloc(32+strlen(wd),sizeof(char));
+        char *phpargs=calloc(256+strlen(wd),sizeof(char));
+        sprintf(php,"%s\\php\\php.exe",wd);
+        sprintf(phpargs,"%s\\php\\php.exe -n -d output_buffering=0 -q install\\install.php usb >nul",wd);
+        launchAndWait(php,phpargs);
+    }
+
+    // rem start mysql database server
+    // :mysql_start
+    // rem echo MySQL Database is trying to start
+    // rem echo Please wait  ....
+    // start "" /B mysql\bin\mysqld --defaults-file=mysql\bin\my.ini --standalone --console >nul
+    // rem timeout  5
+    // ping -n 5 127.0.0.1 >nul
+    char *mysqld=calloc(32+strlen(wd),sizeof(char));
+    char *mysqldargs=calloc(256+strlen(wd),sizeof(char));
+    sprintf(mysqld,"%s\\mysql\\bin\\mysqld.exe",wd);
+    sprintf(mysqldargs,"%s\\mysql\\bin\\mysqld.exe --defaults-file=mysql\\bin\\my.ini --standalone --console >nul",wd);
+    launchAndForget(mysqld,mysqldargs);
+    sleep(7);
+
+    // rem start apache web server
+    // :apache_start
+    // rem echo Starting Apache Web Server....
+    // start "" /B apache\bin\httpd.exe >nul
+    // rem timeout  5
+    // ping -n 5 127.0.0.1 >nul
+    char *apache=calloc(32+strlen(wd),sizeof(char));
+    char *apacheargs=calloc(256+strlen(wd),sizeof(char));
+    sprintf(apache,"%s\\apache\\bin\\httpd.exe",wd);
+    sprintf(apacheargs,"%s\\apache\\bin\\httpdd.exe >nul",wd);
+    launchAndForget(apache,apacheargs);
+    sleep(7);
+
+    /* create database and basic data on first install */
+    // if not exist ..\logs\first_install GOTO browser_start
+    if ( first_install() ) {
+        // rem echo Creating AgilityContest Databases. Please wait
+        //timeout /t 5
+        char *buff=calloc(1024,sizeof(char));
+        FILE *f=fopen("..\\logs\\install.sql","w");
+        FILE *u=fopen("..\\extras\\users.sql","r");
+        sleep(5); // extra timeout for let mysqld extra time to start
+
+        // echo DROP DATABASE IF EXISTS agility; > ..\logs\install.sql
+        // echo CREATE DATABASE agility; >> ..\logs\install.sql
+        // echo USE agility; >> ..\logs\install.sql
+        // rem type ..\extras\agility.sql >> ..\logs\install.sql
+        // type ..\extras\users.sql >> ..\logs\install.sql
+        fputs("DROP DATABASE IF EXISTS agility;\n",f);
+        fputs("CREATE DATABASE agility;\n",f);
+        fputs("USE AGILITY;\n",f);
+        while(fgets(buff,1024,u)!=NULL) fputs(buff,f);
+        fclose(u);
+        fclose(f);
+
+        // rem on first run create database and database users
+        // mysql\bin\mysql -u root < ..\logs\install.sql
+        char *mysql=calloc(32+strlen(wd),sizeof(char));
+        char *mysqlargs=calloc(256+strlen(wd),sizeof(char));
+        sprintf(mysql,"%s\\mysql\\bin\\mysql.exe",wd);
+        sprintf(mysqlargs,"%s\\mysql\\bin\\mysql.exe -u root < ..\\logs\\install.sql",wd);
+        launchAndWait(mysql,mysqlargs);
+    }
+
+    /*
+    del ..\logs\install.sql
+    del ..\logs\first_install
+    rem echo Opening AgilityContest console for first time...
+    start /MAX "AgilityContest" https://localhost/agility/console/index.php?installdb=1
+    goto wait_for_end
+    rem normal start when database is installed
+    :browser_start
+    rem echo Opening AgilityContest console...
+    start /MAX "AgilityContest" https://localhost/agility/console
+    :wait_for_end
+    exit
+    */
+    char *browser="start /MAX \"AgilityContest\" https://localhost/agility/console";
+    if (first_install() ) {
+        browser="start /MAX \"AgilityContest\" https://localhost/agility/console/index.php?installdb=1";
+    }
+    // del ..\logs\install.sql
+    // del ..\logs\first_install
+    unlink("..\\logs\\install.sql");
+    unlink("..\\logs\\first_install.sql");
+    system(browser);
+    return 0;
 }
-/*
 
-
-
-rem if required prepare portable xampp to properly setup directories
-if not exist ..\logs\first_install GOTO mysql_start
-rem echo Configuring first boot of XAMPP
-set PHP_BIN=php\php.exe
-set CONFIG_PHP=install\install.php
-%PHP_BIN% -n -d output_buffering=0 -q %CONFIG_PHP% usb >nul
-
-rem start mysql database server
-:mysql_start
-rem echo MySQL Database is trying to start
-rem echo Please wait  ....
-start "" /B mysql\bin\mysqld --defaults-file=mysql\bin\my.ini --standalone --console >nul
-rem timeout  5
-ping -n 5 127.0.0.1 >nul
-
-rem start apache web server
-:apache_start
-rem echo Starting Apache Web Server....
-start "" /B apache\bin\httpd.exe >nul
-rem timeout  5
-ping -n 5 127.0.0.1 >nul
-
-rem on first run create database and database users
-if not exist ..\logs\first_install GOTO browser_start
-rem echo Creating AgilityContest Databases. Please wait
-timeout /t 5
-echo DROP DATABASE IF EXISTS agility; > ..\logs\install.sql
-echo CREATE DATABASE agility; >> ..\logs\install.sql
-echo USE agility; >> ..\logs\install.sql
-rem type ..\extras\agility.sql >> ..\logs\install.sql
-type ..\extras\users.sql >> ..\logs\install.sql
-mysql\bin\mysql -u root < ..\logs\install.sql
-del ..\logs\install.sql
-del ..\logs\first_install
-rem echo Opening AgilityContest console for first time...
-start /MAX "AgilityContest" https://localhost/agility/console/index.php?installdb=1
-goto wait_for_end
-
-rem normal start when database is installed
-:browser_start
-rem echo Opening AgilityContest console...
-start /MAX "AgilityContest" https://localhost/agility/console
-
-:wait_for_end
-exit
-*/
