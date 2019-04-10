a410bbb6caa44c6088af3fcc68f9e6321358ef15
jonsito <jonsito@gmail.com>
2019-04-10 22:46:55 +0200

    SuTurno: now client and server talks. time to do something useful

--- applications/RaspBerryPi_NowRunning/NRWeb.py
+++ applications/RaspBerryPi_NowRunning/NRWeb.py
@@ -2,64 +2,116 @@ import socketserver
 # for python2
 # from BaseHTTPServer import BaseHTTPRequestHandler
 # for python3
-from http.server import BaseHTTPRequestHandler
-
 import time
 import threading
+import cgi
+from os import curdir,sep
+from http.server import BaseHTTPRequestHandler
+
 
 class MyHandler(BaseHTTPRequestHandler):
-    def read_data(self):
-        print("read data")
+	def readData(self):
+		print("read data")
+		# pending: retrieve all required parameters
+		return '{"success":true}'
 
-    def write_data(self):
-        print("Write_data")
+	def writeData(self,form):
+		print("Write_data")
+		# pending: set all received parameters
+		for item in form:
+		    print("Key: %s Value:%s" %(item,form[item].value) )
 
-    def _set_headers(self):
-        self.send_response(200)
-        #self.send_header('Content-type', 'application/json')
-        self.send_header('Content-type', 'text/html')
-        self.end_headers()
+	def do_GET(self):
+		if self.path=="/":
+			self.path="/index.html"
 
-    def do_GET(self):
-        if self.path == '/readData':
-            read_data()
-        elif self.path == '/writeData':
-            write_data()
-        else:
-            BaseHTTPRequestHandler.do_GET()
-        self._set_headers()
+		try:
+			#Check the file extension required and
+			#set the right mime type
 
-    def do_POST(self):
-        # '''Reads post request body'''
-        self._set_headers()
-        content_len = int(self.headers.getheader('content-length', 0))
-        post_body = self.rfile.read(content_len)
-        self.wfile.write("received post request:<br>{}".format(post_body))
+			sendFile = False
+			str=""
+			mimetype='text/html'
+			if self.path.startswith( '/readData'):
+				mimetype='application/json'
+				str=self.readData()
+			if self.path.endswith(".html"):
+				mimetype='text/html'
+				sendFile = True
+			if self.path.endswith(".jpg"):
+				mimetype='image/jpg'
+				sendFile = True
+			if self.path.endswith(".gif"):
+				mimetype='image/gif'
+				sendFile = True
+			if self.path.endswith(".png"):
+				mimetype='image/png'
+				sendFile = True
+			if self.path.endswith(".js"):
+				mimetype='application/javascript'
+				sendFile = True
+			if self.path.endswith(".css"):
+				mimetype='text/css'
+				sendFile = True
 
-    def do_PUT(self):
-        self.do_POST()
+			# try to read file
+			data =str.encode()
+			if sendFile == True:
+				#Open the static file requested and send it
+				f = open(curdir + sep + self.path,'rb')
+				data=f.read()
+				f.close()
+
+			# prepare headers
+			self.send_response(200)
+			self.send_header('Content-type',mimetype)
+			self.end_headers()
+			self.wfile.write(data)
+			return
+
+		except IOError:
+			self.send_error(404,'File Not Found: %s' % self.path)
+
+	def do_POST(self):
+		if self.path=="/writeData":
+			form = cgi.FieldStorage(
+				fp=self.rfile,
+				headers=self.headers,
+				environ={'REQUEST_METHOD':'POST',
+		                 'CONTENT_TYPE':self.headers['Content-Type'],
+			})
+			self.writeData(form)
+			data=self.readData()
+			self.send_response(200)
+			self.send_header('Content-type','application/json')
+			self.end_headers()
+			self.wfile.write(data.encode())
+			return
+
+	def do_PUT(self):
+		self.do_POST()
 
 class NRWeb:
-    loop = True
+	loop = True
 
-    def stopWeb(self):
-        NRWeb.loop=False
+	def stopWeb(self):
+		NRWeb.loop=False
 
-    def webLoop(self):
-        server_thread = threading.Thread(target=self.httpd.serve_forever)
-        # Exit the server thread when the main thread terminates
-        server_thread.daemon = True
-        server_thread.start()
-        # as using threads for clients, main thread only monitorizes end of loop, so just sleep and wait
-        while NRWeb.loop == True:
-            time.sleep(5) # check for end every 5 seconds,
-        self.httpd.shutdown()
-        self.httpd.server_close()
+	def webLoop(self):
+		server_thread = threading.Thread(target=self.httpd.serve_forever)
+		# Exit the server thread when the main thread terminates
+		server_thread.daemon = True
+		server_thread.start()
+		# as using threads for clients, main thread only monitorizes end of loop, so just sleep and wait
+		while NRWeb.loop == True:
+			time.sleep(5) # check for end every 5 seconds,
+		self.httpd.shutdown()
+		self.httpd.server_close()
 
-        # while NRWen.loop == True
+		# while NRWen.loop == True
 
-    def __init__(self):
-        handler=MyHandler
-        self.httpd = socketserver.TCPServer(("", 8000), handler)
-        self.httpd.server_name = "localhost"
-        self.httpd.server_port = 8000
+	def __init__(self):
+		handler=MyHandler
+		self.httpd = socketserver.TCPServer(("", 8000), handler)
+		self.httpd.server_name = "localhost"
+		self.httpd.server_port = 8000
