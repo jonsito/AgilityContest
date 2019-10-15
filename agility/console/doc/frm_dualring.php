e956b0ea6ff983a1a4fb1aeaf5da24fb3fd16fb6
Juan Antonio Martinez <jonsito@gmail.com>
2019-10-15 15:07:26 +0200

    Documentation: finally is up and running. Yeah!

---
+++ agility/console/doc/frm_dualring.php
@@ -0,0 +1,8 @@
+
+<iframe
+        src="../../ajax/fileFunctions.php?Operation=download&File=ac_dual_ring.pdf&Data=0#page=1"
+        style="border:none;padding-top:50px;width:900px;height:500px;overflow:hidden;" scrolling="no">
+    This browser does not support embedded PDFs. Please manually download the PDF to view it:
+    <a href="http://www.agilitycontest.es/downloads/ac_dual_ring.pdf#page=1">Download PDF</a>
+</iframe>
+
---
+++ agility/console/doc/frm_mail.php
@@ -0,0 +1,8 @@
+
+<iframe
+        src="../../ajax/fileFunctions.php?Operation=download&File=ac_manual_de_correo_electronico.pdf&Data=0#page=29"
+        style="border:none;padding-top:50px;width:900px;height:500px;overflow:hidden;" scrolling="no">
+    This browser does not support embedded PDFs. Please manually download the PDF to view it:
+    <a href="http://www.agilitycontest.es/downloads/ac_manual_de_correo_electronico.pdf#page=29">Download PDF</a>
+</iframe>
+
