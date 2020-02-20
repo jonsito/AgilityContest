bb97979d79a6f6c77e35a7076304bae7245e65d2
Juan Antonio Martinez <jonsito@gmail.com>
2020-02-20 14:45:03 +0100

    Some works in blog php file, to allow partial data returns
    Just add checkpoints for timestamp, to show entire page ( if called from web )
    or just requested items (if called from AgilityContest)

--- web/blog/index.php
+++ web/blog/index.php
@@ -1,3 +1,13 @@
+<?php
+define("AC_EPOCH","blog_1900-01-01_00:00:00");
+$from=AC_EPOCH;
+if (array_key_exists("TimeStamp",$_REQUEST)) {
+    $from="blog-{$_REQUEST['TimeStamp']}";
+}
+?>
+
+<?php if ($from==AC_EPOCH) { ?>
+
 <html lang="es">
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
@@ -8,13 +18,116 @@
 </head>
 <body>
 
-<span style="float:left">
-    <p style="font: italic bold 20px/30px Georgia, serif;">AgilytyContest News &amp; Blog</p>
-</span>
+<div style="display:inline-block;width:100%">
+    <span style="float:left">
+        <p style="font: italic bold 20px/30px Georgia, serif;">AgilytyContest News &amp; Blog</p>
+    </span>
+    <span style="float:right">
+        <img alt="logo" src="https://raw.github.com/jonsito/AgilityContest/master/agility/images/AgilityContest.png">
+    </span>
+</div>
+    <a id="top"></a>
+<?php } ?>
 
-<span style="float:right">
-     <img alt="logo" src="https://raw.github.com/jonsito/AgilityContest/master/agility/images/AgilityContest.png">
-</span>
+
+<?php if ( strcmp($from,"blog_2020-03-01_00:00:00")<0) { ?>
+<strong>2020-Mar-01 00:00:00</strong><br/>
+2<br/>
+3<br/>
+4<br/>
+5<br/><img alt="logo" src="https://raw.github.com/jonsito/AgilityContest/master/agility/images/AgilityContest.png">
+6<br/>
+7<br/>
+8<br/>
+9<br/>
+10<br/>
+<?php } ?>
+
+
+<?php if ( strcmp($from,"blog_2020-02-20_00:00:00")<0) { ?>
+<strong>2020-Feb-20 00:00:00</strong><br/>
+11<br/>
+12<br/>
+13<br/>
+14<br/>
+15<br/><img alt="logo" src="https://raw.github.com/jonsito/AgilityContest/master/agility/images/AgilityContest.png">
+16<br/>
+17<br/>
+18<br/>
+19<br/>
+20<br/>
+<?php } ?>
+
+
+<?php if ( strcmp($from,"blog_2020-02-10_00:00:00")<0) { ?>
+<strong>2020-Feb-10 00:00:00</strong><br/>
+21<br/>
+22<br/>
+23<br/>
+24<br/>
+25<br/>
+26<br/>
+27<br/>
+28<br/>
+29<br/>
+30<br/>
+<?php } ?>
+
+
+<?php if ( strcmp($from,"blog_2020-01-30_00:00:00")<0) { ?>
+<strong>2020-Jan-30 00:00:00</strong><br/>
+31<br/>
+32<br/>
+33<br/>
+34<br/>
+35<br/>
+36<br/>
+37<br/>
+38<br/>
+39<br/>
+40<br/>
+<?php } ?>
+
+
+<?php if ( strcmp($from,"blog_2020-01-20_00:00:00")<0) { ?>
+<strong>2020-Jan-20 00:00:00</strong><br/>
+41<br/>
+42<br/>
+43<br/>
+44<br/>
+45<br/>
+46<br/>
+47<br/>
+48<br/>
+49<br/>
+50<br/>
+<?php } ?>
+
+
+<?php if ( strcmp($from,"blog_2020-01-10_00:00:00")<0) { ?>
+<strong>2020-Jan-10 00:00:00</strong><br/>
+51<br/>
+52<br/>
+53<br/>
+54<br/>
+55<br/>
+56<br/>
+57<br/>
+58<br/>
+59<br/>
+60<br/>
+<?php } ?>
+
+
+<?php if ( strcmp($from,"blog_2020-01-01_00:00:00")<0) { ?>
+<strong>2020-Jan-01 00:00:00</strong><br/>
+61<br/>
+62<br/>
+<?php } ?>
+
+<?php if ($from==AC_EPOCH) { ?>
+<a id="bottom"></a>
 
 </body>
 </html>
+<?php } ?>
