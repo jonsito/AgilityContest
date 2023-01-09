531b66a3063d092967c7c75e5fd7b57b44812759
jantonio <jonsito@gmail.com>
2023-01-09 19:31:49 +0100

    fix entradaDeDatosVacia pdf
    PENDING: need to revise every preg_match()!==FALSE, that may return false or zero

--- agility/server/pdf/classes/PrintTRSTemplates.php
+++ agility/server/pdf/classes/PrintTRSTemplates.php
@@ -225,7 +225,7 @@ class PrintTRSTemplates extends PrintCommon {
         // indicamos nombre del operador que rellena la hoja
         $this->ac_header(2,12);
         $this->Cell(90,7,_('Record by').':','LTBR',0,'L',true);
-        $this->Cell(10,7,'',0,'L',false);
+        $this->Cell(10,7,'','',0,'L',false);
         $this->Cell(90,7,_('Review by').':','LTBR',0,'L',true);
         $this->Ln(15);
         $wide=$this->federation->hasWideLicense(); // use long cell for license when required
